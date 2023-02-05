5f6be8e932e99b6aa7ceb405d37be7b18ade2f95
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-05 03:15:15 -0500

    added code into js file for Countin Sheep question

--- CodeWars/17-Counting-Sheep.js
+++ CodeWars/17-Counting-Sheep.js
@@ -1,1 +1,11 @@
-function countSheeps(arrayOfSheep) {}
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+
+  let sheepCheck = (element) => {
+    if (element === true) {
+      count++;
+    }
+  };
+  arrayOfSheep.forEach(sheepCheck);
+  return count;
+}
