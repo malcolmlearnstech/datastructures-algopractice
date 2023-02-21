67e1c77fb1cbc6393fe5671ab5308197291f2787
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-20 23:50:29 -0500

    entered initial parts of solution for Ziiiip question in js file

--- CodeWars/23-Ziiiip.js
+++ CodeWars/23-Ziiiip.js
@@ -1,1 +1,15 @@
-function zipObject(keys, values) {}
+function zipObject(keys, values) {
+  let resultingObject = {};
+
+  if (!keys && !values) {
+    return resultingObject;
+  }
+
+  if (keys && values) {
+    for (i = 0; i < keys.length; i++) {
+      let property = keys[i];
+      let inputData = values[i];
+      resultingObject[property] = inputData;
+    }
+  }
+}
