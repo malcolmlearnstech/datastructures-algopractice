9651a03c7c05de5e58f2aa4da6afe7b5bab81840
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-20 23:51:14 -0500

    added final conditional check and return for Ziiiip question

--- CodeWars/23-Ziiiip.js
+++ CodeWars/23-Ziiiip.js
@@ -12,4 +12,19 @@ function zipObject(keys, values) {
       resultingObject[property] = inputData;
     }
   }
+
+  if (keys && !values) {
+    for (i = 0; i < keys.length; i++) {
+      let property = keys[i];
+      if (property.length > 1) {
+        let inputData = property[1];
+        property = property[0];
+        resultingObject[property] = inputData;
+      } else {
+        property = property[0];
+        resultingObject[property] = undefined;
+      }
+    }
+  }
+  return resultingObject;
 }
