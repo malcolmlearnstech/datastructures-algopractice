14a0a1e8d22550dd4a48ea904d77a21907cff0fa
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-28 17:53:29 -0500

    added solution code to js file

--- CodeWars/27-Exes-And-Ohs.js
+++ CodeWars/27-Exes-And-Ohs.js
@@ -1,1 +1,9 @@
-function XO(str) {}
+function XO(str) {
+  let array = str.toLowerCase().split('');
+  let xCheck = array.filter((letter) => letter === 'x');
+  let oCheck = array.filter((letter) => letter === 'o');
+
+  if (xCheck.length === oCheck.length) {
+    return true;
+  } else return false;
+}
