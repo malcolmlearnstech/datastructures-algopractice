62d9d0ba86e3a5c4bcd9cb522460f678cb35a30d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-05 23:13:45 -0500

    added optimized solution code to js file for Detect Pangram

--- CodeWars/30-Detect-Pangram.js
+++ CodeWars/30-Detect-Pangram.js
@@ -11,3 +11,17 @@ function isPangram(string) {
   }
   return true;
 }
+
+// REFACTORED/SLIGHTLY OPTIMIZED VERSION OF ORIGINAL SOLUTION
+
+function isPangram(string) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let cleanString = string.toLowerCase();
+
+  for (letter of alphabet) {
+    if (!cleanString.includes(letter)) {
+      return false;
+    }
+  }
+  return true;
+}
