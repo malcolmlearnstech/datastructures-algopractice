97a3e725d7af0731db33ed1e710e625ad02a29ab
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-07 23:38:03 -0500

    added solution code to Count Divisors

--- CodeWars/31-Count-Divisors.js
+++ CodeWars/31-Count-Divisors.js
@@ -1,1 +1,12 @@
-function getDivisorsCnt(n) {}
+function getDivisorsCnt(n) {
+  let divisor = 1;
+  let count = 0;
+
+  while (divisor <= n) {
+    if (n % divisor === 0) {
+      count += 1;
+    }
+    divisor++;
+  }
+  return count;
+}
