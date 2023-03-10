4f735cfabe6be36902d0f61ec15ec597cb002b36
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-09 23:51:01 -0500

    added initial solution code to js file for Two to One

--- CodeWars/32-Two-to-One.js
+++ CodeWars/32-Two-to-One.js
@@ -1,1 +1,19 @@
-function longest(s1, s2) {}
+function longest(s1, s2) {
+  let newString1 = s1.split('').sort();
+  let newString2 = s2.split('').sort();
+  let returnString = [];
+
+  for (const element of newString1) {
+    if (!newString2.includes(element) && !returnString.includes(element)) {
+      returnString.push(element);
+    }
+  }
+
+  for (const element of newString2) {
+    if (!returnString.includes(element)) {
+      returnString.push(element);
+    }
+  }
+
+  return returnString.sort().join('');
+}
