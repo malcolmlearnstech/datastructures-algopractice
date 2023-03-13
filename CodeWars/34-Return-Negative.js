8ab448eaa6bb155f39ab8180dd2570f8f48b4d81
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-13 15:53:45 -0400

    added refactored solution code

--- CodeWars/34-Return-Negative.js
+++ CodeWars/34-Return-Negative.js
@@ -9,3 +9,7 @@ REFACTORED CODE - We are checking for the same condition, in this instance
 we just use a ternary operator instead; allowing for the condition and both
 outcomes to be held on one line.
 */
+
+function makeNegative(num) {
+  return Math.sign(num) === 1 ? num * -1 : num;
+}
