d809725426fa1a96ce608ee2f8613ba7de8d68fe
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-12 23:48:48 -0400

    added solution code to js file for Find Unique

--- CodeWars/33-Find-the-Unique.js
+++ CodeWars/33-Find-the-Unique.js
@@ -1,1 +1,9 @@
-function findUniq(arr) {}
+function findUniq(arr) {
+  let unique = 0;
+  arr.forEach((element) => {
+    if (arr.indexOf(element) === arr.lastIndexOf(element)) {
+      unique += element;
+    }
+  });
+  return unique;
+}
