6359508dc29c52576f4282b7c42c877cd5a985c4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-12 23:56:36 -0400

    added explanation of second code refactor in js file

--- CodeWars/33-Find-the-Unique.js
+++ CodeWars/33-Find-the-Unique.js
@@ -11,7 +11,7 @@ function findUniq(arr) {
 /*
 REFACTORED CODE - We essential perform the same check as above code, but we place the conditions
 of the check on each number as the filtering condition, because the return would be an element in
-an array, we then wrap the return in a to Number method
+an array, we then wrap the return in a to Number method.
 */
 function findUniq(arr) {
   return Number(
@@ -19,6 +19,10 @@ function findUniq(arr) {
   );
 }
 
+/*
+REFACTORED CODE - We perform the same filtering action as the above refactor. We simply access the number
+the single element held in the filter's array return by using bracket access.
+*/
 function findUniq(arr) {
   return arr.filter(
     (element) => arr.indexOf(element) === arr.lastIndexOf(element)
