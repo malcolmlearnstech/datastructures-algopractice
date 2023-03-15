a80205cc261c2ef08e9a49a2f59882a69708e25a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-14 23:46:02 -0400

    added solution code to js file for Hex String to RGB

--- CodeWars/35-Hex-String-to-RGB.js
+++ CodeWars/35-Hex-String-to-RGB.js
@@ -1,1 +1,27 @@
-function hexStringToRGB(hexString) {}
+function hexStringToRGB(hexString) {
+  let numsOnly = hexString.split('').splice(1);
+  let array = [[], [], []];
+  let newArray = [];
+  let i = 0;
+
+  numsOnly.forEach((element) => {
+    if (array[i].length < 2) {
+      array[i].push(element);
+    } else {
+      array[i] = array[i].join('');
+      i++;
+      array[i].push(element);
+    }
+  });
+
+  if (i === array.length - 1) {
+    array[i] = array[i].join('');
+  }
+  for (let i = 0; i < array.length; i++) {
+    array[i] = parseInt(array[i], 16);
+  }
+
+  let object = { r: array[0], g: array[1], b: array[2] };
+
+  return object;
+}
