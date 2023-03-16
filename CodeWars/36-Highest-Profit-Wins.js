82fec2cb55f27795a8ad7f90bc2438df4b509a92
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-15 23:51:32 -0400

    chore: added explanation of refactored solution to js file

--- CodeWars/36-Highest-Profit-Wins.js
+++ CodeWars/36-Highest-Profit-Wins.js
@@ -9,6 +9,7 @@ function minMax(arr) {
 
 /*  REFACTORED CODE:
 
+Because we know that the solution will always only have two elements (one for lowest and one for highest), we can place the Math.min and Math.max directly into an array; as opposed to two separate lines of array pushes.
 */
 function minMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
