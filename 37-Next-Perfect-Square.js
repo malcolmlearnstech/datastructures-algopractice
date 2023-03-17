d5b6af5f5e68c4635fef8573fc546326f2611004
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-17 12:31:49 -0400

    chore: added refactored solutions to js file

--- 37-Next-Perfect-Square.js
+++ 37-Next-Perfect-Square.js
@@ -3,3 +3,13 @@ function findNextSquare(sq) {
     return Math.pow(Math.sqrt(sq) + 1, 2);
   } else return -1;
 }
+
+function findNextSquare(sq) {
+  return Number.isInteger(Math.sqrt(sq))
+    ? (Math.sqrt(sq) + 1) * (Math.sqrt(sq) + 1)
+    : -1;
+}
+
+function findNextSquare(sq) {
+  return Number.isInteger(Math.sqrt(sq)) ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
+}
