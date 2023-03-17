b4e908d39cb5fdd4a8bf043bcda9e34fd7b2749c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-17 12:36:54 -0400

    chore: added explanation of refactored solutions to js file

--- 37-Next-Perfect-Square.js
+++ 37-Next-Perfect-Square.js
@@ -4,12 +4,20 @@ function findNextSquare(sq) {
   } else return -1;
 }
 
+/*  REFACTORED CODE:
+
+Essentially doing the same process, we just use a ternary operator instead. We also perform square root then add 1 to find next number, then we multiply that result by itself.
+*/
 function findNextSquare(sq) {
   return Number.isInteger(Math.sqrt(sq))
     ? (Math.sqrt(sq) + 1) * (Math.sqrt(sq) + 1)
     : -1;
 }
 
+/*  REFACTORED CODE:
+
+This refactor is the same as the prior, aside from one piece. We use the power of operator instead of hard coding the math. Math.pow method allows you to tell the method how many times a number should be multiplied by itself.
+*/
 function findNextSquare(sq) {
   return Number.isInteger(Math.sqrt(sq)) ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
 }
