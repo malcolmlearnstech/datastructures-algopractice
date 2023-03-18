f04c81e3368bcb24c77d1f5516700117789df6c1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-18 19:29:55 -0400

    chore: added solution code to js file

--- CodeWars/38-List-Filtering.js
+++ CodeWars/38-List-Filtering.js
@@ -1,1 +1,10 @@
-function filterList(l) {}
+function filterList(l) {
+  let newList = [];
+
+  l.forEach((element) => {
+    if (typeof element === 'number') {
+      newList.push(element);
+    }
+  });
+  return newList;
+}
