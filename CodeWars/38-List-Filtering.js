ef42a8f555d6084d16f77b2fd1463942eb19a319
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-18 19:52:47 -0400

    chore: added explanation for refactored code

--- CodeWars/38-List-Filtering.js
+++ CodeWars/38-List-Filtering.js
@@ -9,6 +9,10 @@ function filterList(l) {
   return newList;
 }
 
+/*  REFACTORED CODE:
+
+Because we know what we are supposed to seek out, we can now use the conditional from our inital code's if and use that as the basis for what we filter out.
+*/
 function filterList(l) {
   return l.filter((element) => typeof element === 'number');
 }
