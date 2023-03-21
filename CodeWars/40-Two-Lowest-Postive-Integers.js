2bb8d11fcfaad22fa9fb53cf23fa3c477699e3da
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-20 23:04:12 -0400

    chore: added explanation of refactored code solutions

--- CodeWars/40-Two-Lowest-Postive-Integers.js
+++ CodeWars/40-Two-Lowest-Postive-Integers.js
@@ -7,7 +7,12 @@ function sumTwoSmallestNumbers(numbers) {
   return numbers[0] + numbers[1];
 }
 
-//REFACTORED SOLUTION
+/*  REFACTORED CODE:
+
+Rather than building out the sort behavior in a separate function outside of the solution code's scope, 
+we move the code build for function sorting and place it within the method 'sort' to define sort's behavior.
+
+*/
 function sumTwoSmallestNumbers(numbers) {
   numbers.sort((a, b) => {
     return a - b;
@@ -16,7 +21,12 @@ function sumTwoSmallestNumbers(numbers) {
   return numbers[0] + numbers[1];
 }
 
-//REFACTORED SOLUTION
+/*  REFACTORED CODE:
+
+We define the behavior of 'sort' without the use of return and place this in the variable 'numbers',
+setting it to the newly sorted result
+
+*/
 function sumTwoSmallestNumbers(numbers) {
   numbers = numbers.sort((a, b) => a - b);
 
