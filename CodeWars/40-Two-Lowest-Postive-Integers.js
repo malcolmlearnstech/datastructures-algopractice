3c94b19c727e96f0a35f970425fb1a933a06b798
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-20 22:41:09 -0400

    chore: added refactored solutions to js file

--- CodeWars/40-Two-Lowest-Postive-Integers.js
+++ CodeWars/40-Two-Lowest-Postive-Integers.js
@@ -6,3 +6,19 @@ function sumTwoSmallestNumbers(numbers) {
   numbers = numbers.sort(sorting);
   return numbers[0] + numbers[1];
 }
+
+//REFACTORED SOLUTION
+function sumTwoSmallestNumbers(numbers) {
+  numbers.sort((a, b) => {
+    return a - b;
+  });
+
+  return numbers[0] + numbers[1];
+}
+
+//REFACTORED SOLUTION
+function sumTwoSmallestNumbers(numbers) {
+  numbers = numbers.sort((a, b) => a - b);
+
+  return numbers[0] + numbers[1];
+}
