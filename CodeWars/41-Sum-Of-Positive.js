92bd3a4130b722d7842ada44ca1d28714caec460
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-21 20:35:35 -0400

    chore: added solution code to js file

--- CodeWars/41-Sum-Of-Positive.js
+++ CodeWars/41-Sum-Of-Positive.js
@@ -1,1 +1,9 @@
-function positiveSum(arr) {}
+function positiveSum(arr) {
+  let sum = 0;
+  arr.forEach((element) => {
+    if (Math.sign(element) === 1) {
+      sum += element;
+    }
+  });
+  return sum;
+}
