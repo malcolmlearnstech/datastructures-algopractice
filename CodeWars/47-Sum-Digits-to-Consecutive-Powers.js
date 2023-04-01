2496c7641bd3d1d4795463531a062cdd6001fd2d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-01 18:33:03 -0400

    chore: add refactored code to js file

--- CodeWars/47-Sum-Digits-to-Consecutive-Powers.js
+++ CodeWars/47-Sum-Digits-to-Consecutive-Powers.js
@@ -18,3 +18,29 @@ function sumDigPow(a, b) {
 
   return array;
 }
+
+/*REFACTORED CODE SOLUTION: 
+
+Rather than using a for-loop to iterate through our number's digits, we choose to map instead, basically keeping all other elements of the function build the same 
+
+*/
+function sumDigPow(a, b) {
+  let start = a;
+  let array = [];
+
+  while (start < b) {
+    let startSplit = String(start).split('');
+    let sum = 0;
+
+    startSplit.forEach((element, i) => {
+      sum += Math.pow(Number(startSplit[i]), i + 1);
+    });
+
+    if (sum < b && sum === start) {
+      array.push(start);
+    }
+    start++;
+  }
+
+  return array;
+}
