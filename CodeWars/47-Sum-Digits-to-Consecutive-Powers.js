4d10418add8252e2e963ff35b1ec3f3db9f8a3cd
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-01 12:50:49 -0400

    chore: add solution code to js file

--- CodeWars/47-Sum-Digits-to-Consecutive-Powers.js
+++ CodeWars/47-Sum-Digits-to-Consecutive-Powers.js
@@ -1,1 +1,20 @@
-function sumDigPow(a, b) {}
+function sumDigPow(a, b) {
+  let start = a;
+  let array = [];
+
+  while (start < b) {
+    let startSplit = String(start).split('');
+    let sum = 0;
+
+    for (let i = 0; i < startSplit.length; i++) {
+      sum += Math.pow(Number(startSplit[i]), i + 1);
+    }
+
+    if (sum < b && sum === start) {
+      array.push(start);
+    }
+    start++;
+  }
+
+  return array;
+}
