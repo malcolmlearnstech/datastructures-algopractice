cf79acb4c2c29e41fc71b8aa092f4972df445f19
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-03 23:49:50 -0400

    chore: add solution code to js file

--- CodeWars/49-Highest-and-Lowest.js
+++ CodeWars/49-Highest-and-Lowest.js
@@ -1,1 +1,10 @@
-function highAndLow(numbers) {}
+function highAndLow(numbers) {
+  let brokenUp = numbers
+    .match(/-?\d+/g)
+    .map(Number)
+    .sort((a, b) => a - b);
+  let minimum = brokenUp[0];
+  let maximum = brokenUp[brokenUp.length - 1];
+
+  return `${maximum} ${minimum}`;
+}
