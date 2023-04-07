0b80e5c43782e7d60f8600e417dfb1885c0999cc
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-06 23:54:03 -0400

    chore: add solution code to js file

--- CodeWars/51-Ones-and-Zeros.js
+++ CodeWars/51-Ones-and-Zeros.js
@@ -1,1 +1,12 @@
-const binaryArrayToNumber = (arr) => {};
+const binaryArrayToNumber = (arr) => {
+  let multiplier = 1;
+  let sum = 0;
+  let i = arr.length - 1;
+
+  while (i >= 0) {
+    sum += arr[i] * multiplier;
+    multiplier *= 2;
+    i--;
+  }
+  return sum;
+};
