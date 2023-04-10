362b42bd7646adbbdf89535e20213688e50db74c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-09 23:58:40 -0400

    chore: add refactored solution along with explanation

--- CodeWars/52-Equal-Sides-of-an-Array.js
+++ CodeWars/52-Equal-Sides-of-an-Array.js
@@ -29,3 +29,25 @@ function findEvenIndex(arr) {
   }
   return -1;
 }
+
+/*
+REFACTORED SOLUTION:
+
+Because we are performing the the methods up to four times, 
+we see that there is a way we can set the chained methods in 
+"leftSide" and "rightSide" and simply run our comparison last 
+while accounting for every index (including the first and last indices)
+*/
+for (let i = 0; i < arr.length; i++) {
+  let leftSide = arr
+    .slice(0, i)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  let rightSide = arr
+    .slice(i + 1)
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  if (leftSide === rightSide) {
+    return i;
+  }
+}
+return -1;
