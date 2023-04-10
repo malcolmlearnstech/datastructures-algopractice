29338384d74bf3741c3a7f899c5daadeb45a39c7
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-10 16:53:51 -0400

    chore: add solution code to js file

--- CodeWars/53-Array-Diff.js
+++ CodeWars/53-Array-Diff.js
@@ -1,1 +1,8 @@
-function arrayDiff(a, b) {}
+function arrayDiff(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    if (b.includes(a[i])) {
+      a[i] = 'x';
+    }
+  }
+  return a.filter((element) => element !== 'x');
+}
