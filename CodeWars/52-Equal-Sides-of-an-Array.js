a92f4185932818f7f7ff5129c6bba1db46e5cbaa
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-09 23:15:54 -0400

    chore: add solution code to js file

--- CodeWars/52-Equal-Sides-of-an-Array.js
+++ CodeWars/52-Equal-Sides-of-an-Array.js
@@ -1,1 +1,31 @@
-function findEvenIndex(arr) {}
+function findEvenIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let leftSide = 0;
+    let rightSide = 0;
+
+    if (i === 0) {
+      rightSide +=
+        arr.reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          0
+        ) - arr[i];
+    } else if (i === arr.length - 1) {
+      leftSide +=
+        arr.reduce(
+          (accumulator, currentValue) => accumulator + currentValue,
+          0
+        ) - arr[i];
+    } else {
+      leftSide += arr
+        .slice(0, i)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      rightSide += arr
+        .slice(i + 1)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    }
+    if (leftSide === rightSide) {
+      return i;
+    }
+  }
+  return -1;
+}
