f706c257abddb79fe56e2c51914bf800d0e9a700
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-12 23:45:24 -0400

    chore: add refactored solution code and refactored solution explanation

--- CodeWars/55-Sort-Numbers.js
+++ CodeWars/55-Sort-Numbers.js
@@ -3,3 +3,15 @@ function sortNums(nums) {
     return [];
   } else return nums.sort((a, b) => a - b);
 }
+
+/*
+REFACTORED SOLUTION:
+
+Instead of using the "if, else" syntax for this scenario, we can 
+use a ternary operator and still make use of sort. We can also 
+check for the truthiness of our "nums" input.
+*/
+
+function sortNums(nums) {
+  return nums ? nums.sort((a, b) => a - b) : [];
+}
