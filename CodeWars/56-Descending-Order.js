ccc8f9cc5075b70a6c82d32fdc2498be5c5a21dc
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-13 23:53:43 -0400

    chore: add refactored solutions and explanations to js file

--- CodeWars/56-Descending-Order.js
+++ CodeWars/56-Descending-Order.js
@@ -5,3 +5,31 @@ function descendingOrder(n) {
 
   return Number(numConvert.join(''));
 }
+
+/*
+REFACTORED SOLUTION:
+
+Rather than placing string of number in a variable, we can method 
+chain split to "toString", then perform the sorting in the same manner
+*/
+
+function descendingOrder(n) {
+  return parseInt(
+    n
+      .toString()
+      .split('')
+      .sort((a, b) => b - a)
+      .join('')
+  );
+}
+
+/*
+REFACTORED SOLUTION:
+
+We use Number/toNumber() instead of parseInt and the rest of the method chain
+can bascially remain the same
+*/
+
+function descendingOrder(n) {
+  return Number(String(n).split('').sort().reverse().join(''));
+}
