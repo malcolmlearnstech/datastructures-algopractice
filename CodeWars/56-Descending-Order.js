3d5c708a3d6c2a1fa40d1257f3aceb3a8f5be87d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-13 23:49:49 -0400

    chore: add solution code to js file

--- CodeWars/56-Descending-Order.js
+++ CodeWars/56-Descending-Order.js
@@ -1,1 +1,7 @@
-function descendingOrder(n) {}
+function descendingOrder(n) {
+  let numConvert = String(n);
+
+  numConvert = numConvert.split('').sort((a, b) => b - a);
+
+  return Number(numConvert.join(''));
+}
