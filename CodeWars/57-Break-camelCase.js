fb2400d43f0ed38f27750e1314e8435ab64cfce9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-14 23:49:48 -0400

    chore: add solution code to js file

--- CodeWars/57-Break-camelCase.js
+++ CodeWars/57-Break-camelCase.js
@@ -1,1 +1,12 @@
-function solution(string) {}
+function solution(string) {
+  let capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  string = string.split('');
+
+  for (let i = 0; i < string.length; i++) {
+    if (capitals.includes(string[i])) {
+      string.splice(i, 0, ' ');
+      i += 2;
+    }
+  }
+  return string.join('');
+}
