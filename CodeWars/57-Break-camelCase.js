058577f2b650b7fac22eae9f8e544d69c02a71b4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-14 23:52:50 -0400

    chore: add refactored solution and explanation of refactor

--- CodeWars/57-Break-camelCase.js
+++ CodeWars/57-Break-camelCase.js
@@ -10,3 +10,22 @@ function solution(string) {
   }
   return string.join('');
 }
+
+/*
+REFACTORED SOLUTION:
+
+Rather creating a variable to hold all capital letters; in our conditional, 
+we can check if the current string element is strictly equal to its capitalized form.
+*/
+
+function solution(string) {
+  string = string.split('');
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      string.splice(i, 0, ' ');
+      i += 2;
+    }
+  }
+  return string.join('');
+}
