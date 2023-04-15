9619cbfff78a7b3408bc0c4b8d338a4f4aa2cf8c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-15 19:38:02 -0400

    chore: add solution code to js file

--- CodeWars/58-Friend-or-Foe.js
+++ CodeWars/58-Friend-or-Foe.js
@@ -1,1 +1,10 @@
-function friend(friends) {}
+function friend(friends) {
+  let notFoes = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].length === 4) {
+      notFoes.push(friends[i]);
+    }
+  }
+  return notFoes;
+}
