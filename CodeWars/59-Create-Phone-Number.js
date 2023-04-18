d74b043a39f3c8dceadb0a9ba674d5d70020af2b
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-17 22:39:02 -0400

    chore: add refactored solution and explanation

--- CodeWars/59-Create-Phone-Number.js
+++ CodeWars/59-Create-Phone-Number.js
@@ -5,3 +5,21 @@ function createPhoneNumber(numbers) {
 
   return `(${area}) ${three}-${four}`;
 }
+
+/*
+REFACTORED SOLUTION:
+
+Instead of typing every index of each number in the string 
+interpolation, we can use slice to pull out the chunks of 
+number needed for each section. Then use join to turn each 
+section into a string. Lastly, we still employ string interpolation 
+in the return.
+*/
+
+function createPhoneNumber(numbers) {
+  let area = numbers.slice(0, 3).join('');
+  let three = numbers.slice(3, 6).join('');
+  let four = numbers.slice(6).join('');
+
+  return `(${area}) ${three}-${four}`;
+}
