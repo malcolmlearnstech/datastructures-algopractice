8af3fe3e9bb5d1b42d6d1dd07538aa8a161ec00f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-17 22:25:29 -0400

    chore: add solution code to js file

--- CodeWars/59-Create-Phone-Number.js
+++ CodeWars/59-Create-Phone-Number.js
@@ -1,1 +1,7 @@
-function createPhoneNumber(numbers) {}
+function createPhoneNumber(numbers) {
+  let area = `${numbers[0]}${numbers[1]}${numbers[2]}`;
+  let three = `${numbers[3]}${numbers[4]}${numbers[5]}`;
+  let four = `${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+
+  return `(${area}) ${three}-${four}`;
+}
