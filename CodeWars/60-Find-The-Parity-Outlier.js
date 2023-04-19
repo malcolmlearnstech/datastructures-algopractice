6a22c2c159bc49c4569b4a64ff8247b7909c5029
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-18 22:39:29 -0400

    chore: add second solution refactor and refactor explanation

--- CodeWars/60-Find-The-Parity-Outlier.js
+++ CodeWars/60-Find-The-Parity-Outlier.js
@@ -19,3 +19,19 @@ function findOutlier(integers) {
 
   return oddCount.length > 1 ? evenCount[0] : oddCount[0];
 }
+
+/*
+REFACTORED SOLUTION:
+
+Because evenCount will only have one element in its array if the input has 
+more than one odd numbers, we can use the pop method, as evenCount will only have 
+one element (making it the first AND last element). The same logic can be applied 
+on the oddCount array if there is only ONE odd number in the input array.
+*/
+
+function findOutlier(integers) {
+  let evenCount = integers.filter((element) => element % 2 === 0);
+  let oddCount = integers.filter((element) => element % 2 !== 0);
+
+  return oddCount.length > 1 ? evenCount.pop() : oddCount.pop();
+}
