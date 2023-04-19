8dee1ced51b584f9505c89aa0f38ddb47ae358aa
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-18 22:08:03 -0400

    chore: add initial solution code to js file

--- CodeWars/60-Find-The-Parity-Outlier.js
+++ CodeWars/60-Find-The-Parity-Outlier.js
@@ -1,1 +1,7 @@
-function findOutlier(integers) {}
+function findOutlier(integers) {
+  let evenCount = integers.filter((element) => element % 2 === 0);
+
+  return evenCount.length > 1
+    ? integers.filter((element) => element % 2 !== 0)[0]
+    : evenCount[0];
+}
