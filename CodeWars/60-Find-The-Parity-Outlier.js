1ff4dabc04fe188a391a242896f3843d08b71f82
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-18 22:38:03 -0400

    chore: add initial solution refactor and explanation

--- CodeWars/60-Find-The-Parity-Outlier.js
+++ CodeWars/60-Find-The-Parity-Outlier.js
@@ -5,3 +5,17 @@ function findOutlier(integers) {
     ? integers.filter((element) => element % 2 !== 0)[0]
     : evenCount[0];
 }
+
+/*
+REFACTORED SOLUTION: 
+
+We can shorten the return by placing the integers filter within a 
+variable and making that the count of odd numbers
+*/
+
+function findOutlier(integers) {
+  let evenCount = integers.filter((element) => element % 2 === 0);
+  let oddCount = integers.filter((element) => element % 2 !== 0);
+
+  return oddCount.length > 1 ? evenCount[0] : oddCount[0];
+}
