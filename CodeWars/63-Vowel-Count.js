6192c1306fa9075a729cb7f76ab149645cb10f76
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-24 23:30:24 -0400

    chore: add refactored solution and explanation to readme file

--- CodeWars/63-Vowel-Count.js
+++ CodeWars/63-Vowel-Count.js
@@ -9,3 +9,15 @@ function getCount(str) {
   }
   return count;
 }
+
+/*
+REAFACTORED SOLUTION:
+Instead of using a count variable, we can turn the string into an 
+array and filter just the existing vowels in our string, then return 
+the length of that array.
+*/
+
+function getCount(str) {
+  let vowels = 'aeiou';
+  return str.split('').filter((element) => vowels.includes(element)).length;
+}
