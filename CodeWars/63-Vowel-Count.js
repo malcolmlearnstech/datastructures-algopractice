8941159e1e8a70722dacbf723f286a7c29667b77
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-24 23:26:37 -0400

    chore: add solution code to js file

--- CodeWars/63-Vowel-Count.js
+++ CodeWars/63-Vowel-Count.js
@@ -1,1 +1,11 @@
-function getCount(str) {}
+function getCount(str) {
+  let vowels = 'aeiou';
+  let count = 0;
+  str = str.split('');
+  for (element of str) {
+    if (vowels.includes(element)) {
+      count++;
+    }
+  }
+  return count;
+}
