69ccd8c0b71e61d5d20991e09f8d71c241f6210a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-25 21:44:27 -0400

    chore: add refactored solution & explanation to js file

--- CodeWars/64-Filter-Out-the-Geese.js
+++ CodeWars/64-Filter-Out-the-Geese.js
@@ -8,3 +8,16 @@ function gooseFilter(birds) {
   }
   return nonGeese;
 }
+
+/*
+
+REFACTORED SOLUTION: An alternative to doing a for-of loop, 
+we can run the filter method on the input array and instruct 
+filter to only spit back elements that do not exist in the 
+geese array we've created*/
+
+function gooseFilter(birds) {
+  var geese = ['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher'];
+
+  return birds.filter((element) => !geese.includes(element));
+}
