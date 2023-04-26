34ccd7dc0622f8f035d69c9c29eb6a9fd54fcd91
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-25 21:38:49 -0400

    chore: add solution code to js file

--- CodeWars/64-Filter-Out-the-Geese.js
+++ CodeWars/64-Filter-Out-the-Geese.js
@@ -1,1 +1,10 @@
-function gooseFilter(birds) {}
+function gooseFilter(birds) {
+  var geese = ['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher'];
+  let nonGeese = [];
+  for (const element of birds) {
+    if (!geese.includes(element)) {
+      nonGeese.push(element);
+    }
+  }
+  return nonGeese;
+}
