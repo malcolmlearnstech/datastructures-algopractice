0a6dbf02431182c88d7c4a17a8477ff8b0b83683
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-26 22:28:14 -0400

    chore: add refactored solution and explanation to js file

--- CodeWars/65-Array-Deep-Count.js
+++ CodeWars/65-Array-Deep-Count.js
@@ -12,3 +12,22 @@ function deepCount(array) {
   }
   return elementCount;
 }
+
+/*
+
+REFACTORED SOLUTION:
+We can make use of reduce method to step through each element and we use 
+a ternary checking for whether current element is an array or not. If 
+current is not an array, we can add; if current element is an array, 
+then we make a recursive call.
+*/
+
+function deepCount(array) {
+  return array.reduce(
+    (accumulator, currentValue) =>
+      Array.isArray(currentValue)
+        ? accumulator + deepCount(currentValue)
+        : accumulator,
+    array.length
+  );
+}
