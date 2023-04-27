9a8c497ed4bc8f2aedae3d0ae333eaf2815eccb5
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-26 22:19:44 -0400

    chore: add solution code to js file

--- CodeWars/65-Array-Deep-Count.js
+++ CodeWars/65-Array-Deep-Count.js
@@ -1,1 +1,14 @@
-function deepCount(array) {}
+function deepCount(array) {
+  let elementCount = 0;
+  let i = 0;
+
+  while (i < a.length) {
+    elementCount++;
+
+    if (Array.isArray(a[i])) {
+      elementCount += deepCount(a[i]);
+    }
+    i++;
+  }
+  return elementCount;
+}
