0a75cf5bdbd4f7a05e7987c19ce56f4fa50cc3d7
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-27 23:24:24 -0400

    chore: add refactored solution and explanation of refactor

--- CodeWars/66-Divide-and-Conquer.js
+++ CodeWars/66-Divide-and-Conquer.js
@@ -10,3 +10,23 @@ function divCon(x) {
   }
   return nonStrings - strings;
 }
+
+/*
+REFACTORED SOLUTION:
+
+We can use forEach method as an alternative to the for-of loop. Instead 
+of separating non-string integers and strings into separate variables, 
+we perform what is asked in the prompt (add numbers that are integers 
+and subtract numbers that are strings) on a sinlge variable's value*/
+
+function divCon(x) {
+  let count = 0;
+  x.forEach((element) => {
+    if (typeof element !== 'string') {
+      count += element;
+    } else {
+      count -= parseInt(element);
+    }
+  });
+  return count;
+}
