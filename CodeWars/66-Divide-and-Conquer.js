45e32092b80fc1fce7b176812545d24b0bcbd5f9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-27 23:20:54 -0400

    chore: add solution code to js file

--- CodeWars/66-Divide-and-Conquer.js
+++ CodeWars/66-Divide-and-Conquer.js
@@ -1,1 +1,12 @@
-function divCon(x) {}
+function divCon(x) {
+  let nonStrings = 0;
+  let strings = 0;
+  for (const element of x) {
+    if (typeof element !== 'string') {
+      nonStrings += element;
+    } else {
+      strings += parseInt(element);
+    }
+  }
+  return nonStrings - strings;
+}
