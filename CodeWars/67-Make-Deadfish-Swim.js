a09d6b2a0a67d47216148af04d0460a58be47d9c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-28 21:34:53 -0400

    chore add second refactored solution and explanation of refactor

--- CodeWars/67-Make-Deadfish-Swim.js
+++ CodeWars/67-Make-Deadfish-Swim.js
@@ -56,3 +56,35 @@ function parse(data) {
   }
   return output;
 }
+
+/*
+REFACTORED SOLUTION:
+
+An alternative to storing functions in an object is to 
+follow the behavior but, make use of a switch statement to 
+determine each action as we loop thru the input string
+*/
+
+function parse(data) {
+  let initialValue = 0;
+  let output = [];
+
+  for (let i = 0; i < data.length; i++) {
+    let currentCharacter = data[i];
+    switch (currentCharacter) {
+      case 'i':
+        initialValue++;
+        break;
+      case 'd':
+        initialValue--;
+        break;
+      case 's':
+        initialValue *= initialValue;
+        break;
+      case 'o':
+        output.push(initialValue);
+        break;
+    }
+  }
+  return output;
+}
