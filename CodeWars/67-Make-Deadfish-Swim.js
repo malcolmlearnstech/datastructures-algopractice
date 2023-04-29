d3a82e7456b5cf0f82c7639e81465323f3ef2b21
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-28 21:31:12 -0400

    chore: add refactored solution and explanation of refactor

--- CodeWars/67-Make-Deadfish-Swim.js
+++ CodeWars/67-Make-Deadfish-Swim.js
@@ -23,3 +23,36 @@ function parse(data) {
   }
   return output;
 }
+
+/*
+REFACTORED SOLUTION:
+
+The function values in each property of the object 
+can also be set to perform their actions without the use of "return"
+*/
+
+function parse(data) {
+  let initialValue = 0;
+  let output = [];
+  let commands = {
+    i: function () {
+      initialValue++;
+    },
+    d: function () {
+      initialValue--;
+    },
+    s: function () {
+      initialValue *= initialValue;
+    },
+    o: function () {
+      output.push(initialValue);
+    },
+  };
+
+  for (let i = 0; i < data.length; i++) {
+    if (commands[data[i]]) {
+      commands[data[i]](initialValue);
+    }
+  }
+  return output;
+}
