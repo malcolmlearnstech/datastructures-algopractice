23184b701b1a0d49b2a64e148ce9e9678e488a52
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-28 21:27:29 -0400

    chore: add solution code to js file

--- CodeWars/67-Make-Deadfish-Swim.js
+++ CodeWars/67-Make-Deadfish-Swim.js
@@ -1,1 +1,25 @@
-function parse(data) {}
+function parse(data) {
+  let initialValue = 0;
+  let output = [];
+  let commands = {
+    i: function () {
+      return initialValue++;
+    },
+    d: function () {
+      return initialValue--;
+    },
+    s: function () {
+      return (initialValue *= initialValue);
+    },
+    o: function () {
+      output.push(initialValue);
+    },
+  };
+
+  for (let i = 0; i < data.length; i++) {
+    if (commands[data[i]]) {
+      commands[data[i]](initialValue);
+    }
+  }
+  return output;
+}
