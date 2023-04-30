0e79e98bfa98248a57fee442c34977c667eedaab
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-29 22:29:25 -0400

    chore: added personal solution code to js file

--- Review-with-Madhur/1-Valid-Anagram.js
+++ Review-with-Madhur/1-Valid-Anagram.js
@@ -1,1 +1,14 @@
-function validAnagram(str, target) {}
+function validAnagram(str, target) {
+  if (str.length !== target.length) {
+    return false;
+  }
+  str = str.split('').sort();
+  target = target.split('').sort();
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== target[i]) {
+      return false;
+    }
+  }
+  return true;
+}
