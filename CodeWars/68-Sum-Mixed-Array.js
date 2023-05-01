6ebb62ef67854bc916cf360327b0e39e01173c43
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-30 23:45:56 -0400

    chore: add additional refactor & explanation and edit structure of first refactor

--- CodeWars/68-Sum-Mixed-Array.js
+++ CodeWars/68-Sum-Mixed-Array.js
@@ -14,9 +14,21 @@ Instead of forEach method, we can use map to iterate over each element
 and use Number method to convert every element. Once all numbers are 
 converted, we then use reduce method to iterate over each number and add 
 them up, starting with a initial value of 0.*/
+function sumMix(x) {
+  return x
+    .map((element) => Number(element))
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+/*
+REFACTORED SOLUTION:
 
-return x
-  .map((element) => Number(element))
-  .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
-/**/
+As an alternative to Number method and parseInt method, we can also use 
+the unary plus on each element while mapping. Additionally, not indicating 
+a initial value in our reduce method will automatically set the initial 
+value to 0. 
+*/
+function sumMix(x) {
+  return x
+    .map((element) => +element)
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+}
