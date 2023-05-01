a3ae0d62ada2e7c5894d85220a5a33685829225e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-04-30 23:41:42 -0400

    chore: add refactored solution with its explanation to js file

--- CodeWars/68-Sum-Mixed-Array.js
+++ CodeWars/68-Sum-Mixed-Array.js
@@ -6,3 +6,17 @@ function sumMix(x) {
   });
   return sum;
 }
+
+/*
+REFACTORED SOLUTION:
+
+Instead of forEach method, we can use map to iterate over each element 
+and use Number method to convert every element. Once all numbers are 
+converted, we then use reduce method to iterate over each number and add 
+them up, starting with a initial value of 0.*/
+
+return x
+  .map((element) => Number(element))
+  .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+/**/
