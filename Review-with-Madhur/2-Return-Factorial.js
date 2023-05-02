eac774e3f0c52bb2e8783c0d60ae4ee8d1156090
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-01 22:48:20 -0400

    chore: add explanation of Time & Space complexity to js file

--- Review-with-Madhur/2-Return-Factorial.js
+++ Review-with-Madhur/2-Return-Factorial.js
@@ -5,3 +5,5 @@ function factorial(n) {
   }
   return n * factorial(n - 1);
 }
+//Time Complexity: O(N), because the time for the function to complete is dependent on the input "n"
+//Space Complexity: O(N), because the amount of data in the recursive call stack is dependent on how large the input "n" is
