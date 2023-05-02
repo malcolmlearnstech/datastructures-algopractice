27bd0108cb158520b1b35ca6cf77ad85d1834594
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-02 14:14:01 -0400

    chore: add time & space complexity to js file

--- Review-with-Madhur/3-Fibonacci.js
+++ Review-with-Madhur/3-Fibonacci.js
@@ -5,3 +5,6 @@ function fib(n) {
   }
   return fib(n - 1) + fib(n - 2); // <===RECURSIVE CALLS
 }
+
+//Time Complexity: O(2^N) - this solution will produce a recursive tree of N height, which has 2^ fib() calls
+//Space Complexity: 0(2^N) - solution produces a call stack of 2^N fib() calls in worst case scenario
