229e10dd7b37094af9eb7d86315a74c352fe3463
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-03 21:19:10 -0400

    chore: create js file for Reverse Linked List code challenge

---
+++ Review-with-Madhur/4-Reverse-Linked-List.js
@@ -0,0 +1,24 @@
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function generateList(array) {
+  if (array.length === 0) return null;
+
+  let head = new ListNode(array[0]);
+
+  let current = head;
+  for (let i = 1; i < array.length; i++) {
+    current.next = new ListNode(array[i]);
+    current = current.next;
+  }
+  return head;
+}
+
+let array = [1, 5, 7, 10];
+let linkedList = generateList(array);
+
+function reverse(current) {}
