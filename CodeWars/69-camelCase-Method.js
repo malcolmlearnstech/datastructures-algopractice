247970a69c10dbaa2f8ec05b6f697b381dc7dd43
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-04 23:48:46 -0400

    chore: add solution code to js file

--- CodeWars/69-camelCase-Method.js
+++ CodeWars/69-camelCase-Method.js
@@ -1,1 +1,11 @@
-function camelCase(string) {}
+function camelCase(string) {
+  string = string.split('');
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      string.splice(i, 0, ' ');
+      i += 2;
+    }
+  }
+  return string.join('');
+}
