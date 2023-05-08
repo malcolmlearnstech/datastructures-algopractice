24329a401efd0b33fbdc928bc6feffb477330b27
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-07 23:53:31 -0400

    chore: add refactored solution with explanation

--- CodeWars/71-Shortest-Word.js
+++ CodeWars/71-Shortest-Word.js
@@ -4,3 +4,14 @@ function findShort(s) {
 
   return Math.min(...counts);
 }
+
+/*
+REFACTORED SOLUTION:
+
+We can redefine the input s, which will us to not create a new variable, then run Math.min
+*/
+
+function findShort(s) {
+  s = s.split(' ').map((element) => (element = element.length));
+  return Math.min(...s);
+}
