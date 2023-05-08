14d5dffd48d9f0199ebcad74f1909b395c0cdda9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-08 15:48:42 -0400

    chore: add refactored solution & refactor explanation

--- CodeWars/72-Complementary-DNA.js
+++ CodeWars/72-Complementary-DNA.js
@@ -12,3 +12,24 @@ function DNAStrand(dna) {
   });
   return dna.join('');
 }
+
+/*
+REFACTORED SOLUTION:
+
+All methods used on the input variable 'dna' can be appended together. 
+We can also tell map to reassign as it iterates, as an alternative to 
+the conditional used in the initial solution.
+*/
+
+function DNAStrand(dna) {
+  let chemicals = {
+    A: 'T',
+    C: 'G',
+    T: 'A',
+    G: 'C',
+  };
+  return dna
+    .split('')
+    .map((element) => (element = chemicals[element]))
+    .join('');
+}
