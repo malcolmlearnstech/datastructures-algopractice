1b4019e837ea13709fb5ae2157d966dc1946bf5f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-11 19:54:07 -0400

    chore: add solution code to js file

--- CodeWars/73-Mumbling.js
+++ CodeWars/73-Mumbling.js
@@ -1,1 +1,9 @@
-function accum(s) {}
+function accum(s) {
+  let j = 0;
+  s = s.toUpperCase().split('');
+  for (let i = 0; i < s.length; i++) {
+    s[i] = `${s[i]}${s[i].toLowerCase().repeat(j)}`;
+    j++;
+  }
+  return s.join('-');
+}
