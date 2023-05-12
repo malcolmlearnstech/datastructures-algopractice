10bcdfb8f1d879b6a6ab1c2aa905501d0f94afd1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-11 20:22:25 -0400

    chore: add refactored solution and explanation to js file

--- CodeWars/73-Mumbling.js
+++ CodeWars/73-Mumbling.js
@@ -7,3 +7,19 @@ function accum(s) {
   }
   return s.join('-');
 }
+
+/*
+REFACTORED SOLUTION:
+
+We can make use of map to move thru the split array, and rather 
+than make use of "j" to count positions, we can use "index" in 
+the map parameter, related to the positions.
+*/
+
+function accum(s) {
+  s = s.toUpperCase().split('');
+  s = s.map(
+    (letter, index) => (letter = letter + letter.toLowerCase().repeat(index))
+  );
+  return s.join('-');
+}
