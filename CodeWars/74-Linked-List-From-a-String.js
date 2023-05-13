fdd009e87b3a06616239d6aff569639ce24513cc
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-12 23:44:40 -0400

    chore: add solution code to js file

--- CodeWars/74-Linked-List-From-a-String.js
+++ CodeWars/74-Linked-List-From-a-String.js
@@ -1,1 +1,14 @@
-function parse(string) {}
+function parse(string) {
+  if (string === 'null') return null;
+
+  string = string.split(' -> ').map((element) => Number(element));
+
+  let headNode = new Node(string[0]);
+  let current = headNode;
+
+  for (let i = 1; i < string.length - 1; i++) {
+    current.next = new Node(string[i]);
+    current = current.next;
+  }
+  return headNode;
+}
