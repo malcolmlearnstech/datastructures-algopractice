1498824f2f82e46a200be806217fc19d9a367058
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-17 23:35:28 -0400

    chore: add solution code to js file

--- CodeWars/76-Reverse-Vowels-in-String.js
+++ CodeWars/76-Reverse-Vowels-in-String.js
@@ -1,1 +1,21 @@
-function reverseVowels(str) {}
+function reverseVowels(str) {
+  str = str.split('');
+  let vowels = 'aeiouAEIOU';
+  let strVowels = [];
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (vowels.includes(str[i])) {
+      strVowels.push(str[i]);
+    }
+  }
+
+  let strVowelsIndex = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      str[i] = strVowels[strVowelsIndex];
+      strVowelsIndex++;
+    }
+  }
+  return str.join('');
+}
