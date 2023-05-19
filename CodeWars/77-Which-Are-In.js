112e4d712054c134a54ea95758e006b9f6814282
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-18 23:33:24 -0400

    chore: add solution code to js file

--- CodeWars/77-Which-Are-In.js
+++ CodeWars/77-Which-Are-In.js
@@ -1,1 +1,13 @@
-function inArray(array1, array2) {}
+function inArray(array1, array2) {
+  let inBothStrings = [];
+
+  for (const element of array1) {
+    for (let i = 0; i < array2.length; i++) {
+      if (array2[i].includes(element) && !inBothStrings.includes(element)) {
+        inBothStrings.push(element);
+      }
+    }
+  }
+
+  return inBothStrings.sort();
+}
