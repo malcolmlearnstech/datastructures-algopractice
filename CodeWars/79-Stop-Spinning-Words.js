69b7f2fb72bea7e666694e85a031ce286f861b27
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-20 14:45:25 -0400

    chore: add solution code to js file

--- CodeWars/79-Stop-Spinning-Words.js
+++ CodeWars/79-Stop-Spinning-Words.js
@@ -1,1 +1,12 @@
-function spinWords(string) {}
+function spinWords(string) {
+  let array = [];
+  string = string.split(' ');
+  string.map((element, index) => {
+    if (element.length >= 5) {
+      array.push(string[index].split('').reverse().join(''));
+    } else {
+      array.push(element);
+    }
+  });
+  return array.join(' ');
+}
