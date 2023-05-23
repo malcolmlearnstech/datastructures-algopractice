48379dade5ce3ef7b969815deba9b13e5319a91d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-22 23:52:40 -0400

    chore: add solution code to js file

--- LeetCode/Fast-and-Slow-Pointer/2-Linked-List-Cycle-II.js
+++ LeetCode/Fast-and-Slow-Pointer/2-Linked-List-Cycle-II.js
@@ -5,4 +5,17 @@ class ListNode {
   }
 }
 
-var detectCycle = function (head) {};
+var detectCycle = function (head) {
+  let slow = head;
+  let nodeObject = {};
+
+  while (slow !== null && slow.next !== null) {
+    if (!nodeObject[slow]) {
+      nodeObject[slow] = 1;
+    } else {
+      return slow;
+    }
+    slow = slow.next;
+  }
+  return null;
+};
