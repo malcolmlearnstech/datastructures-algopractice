218596b7aaf4f0a8760b975501665f5910ebdc04
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-25 22:53:30 -0400

    chore: add solution code to js file

--- LeetCode/Sliding-Window/1-Find-K-Beauty-of-Number.js
+++ LeetCode/Sliding-Window/1-Find-K-Beauty-of-Number.js
@@ -1,1 +1,16 @@
-var divisorSubstrings = function (num, k) {};
+var divisorSubstrings = function (num, k) {
+  num = String(num);
+  let left = 0;
+  let right = k - 1;
+  let kBeauty = 0;
+
+  while (right < num.length) {
+    let substring = Number(num.slice(left, right + 1));
+    if (Number(num) % substring === 0) {
+      kBeauty++;
+    }
+    left++;
+    right++;
+  }
+  return kBeauty;
+};
