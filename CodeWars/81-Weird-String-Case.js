b1d2e762d5405661000c359844d5e4b541100c8d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-27 23:40:16 -0400

    chore: add solution code to js file

--- CodeWars/81-Weird-String-Case.js
+++ CodeWars/81-Weird-String-Case.js
@@ -1,1 +1,19 @@
-function toWeirdCase(string) {}
+function toWeirdCase(string) {
+  string = string.toLowerCase().split(' ');
+
+  for (let i = 0; i < string.length; i++) {
+    string[i] = string[i].split('');
+
+    string[i] = string[i]
+      .map((element, index) => {
+        if (index % 2 === 0) {
+          return element.toUpperCase();
+        } else {
+          return element;
+        }
+      })
+      .join('');
+  }
+
+  return string.join(' ');
+}
