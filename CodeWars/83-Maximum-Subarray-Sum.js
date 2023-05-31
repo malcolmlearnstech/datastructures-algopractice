530e6903a811795bcd96e536d77c9319850fdf1e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-30 23:11:55 -0400

    chore: add solution code to js file

--- CodeWars/83-Maximum-Subarray-Sum.js
+++ CodeWars/83-Maximum-Subarray-Sum.js
@@ -1,1 +1,13 @@
-function maxSequence(arr) {}
+function maxSequence(arr) {
+  let currentSum = 0;
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentSum += arr[i];
+    if (currentSum < 0) {
+      currentSum = 0;
+    }
+    maxSum = Math.max(maxSum, currentSum);
+  }
+  return arr.length ? maxSum : 0;
+}
