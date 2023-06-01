72679db501aea5a725806706472ce7e0cbf9906f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-31 21:29:25 -0400

    chore: add solution code to js file

--- CodeWars/84-Switch-It-Up.js
+++ CodeWars/84-Switch-It-Up.js
@@ -1,1 +1,24 @@
-function switchItUp(number) {}
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return 'Zero';
+    case 1:
+      return 'One';
+    case 2:
+      return 'Two';
+    case 3:
+      return 'Three';
+    case 4:
+      return 'Four';
+    case 5:
+      return 'Five';
+    case 6:
+      return 'Six';
+    case 7:
+      return 'Seven';
+    case 8:
+      return 'Eight';
+    default:
+      return 'Nine';
+  }
+}
