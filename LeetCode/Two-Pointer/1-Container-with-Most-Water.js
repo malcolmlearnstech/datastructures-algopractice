d75a6707425fc091ab5dbeafa35a8ba791a1c227
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-09 22:29:16 -0400

    chore: create js file for Container with Most Water question

---
+++ LeetCode/Two-Pointer/1-Container-with-Most-Water.js
@@ -0,0 +1,12 @@
+/*
+APPROACH:
+
+1. initialize left & right at the beginning and end of input array, respectively
+2. initialize a dummy area of 0
+3. calculate area btwn left & right
+    a. area is calculated by smaller of left & right, times left to right distance minus 1
+4. find max btwn initialized dummy area and area calculated btwn left & right
+5. if left is greater than right, move right down; if left is less than right, move left up
+6. perform steps 3 to 5 until left & right cross
+
+*/
