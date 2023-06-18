d2d7dd34db27cf0736ee53783670396a3babe7ca
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-17 23:21:51 -0400

    chore: add solution code to js file

--- CodeWars/85-Find-Cracker.js
+++ CodeWars/85-Find-Cracker.js
@@ -1,1 +1,34 @@
-function findHack(arr) {}
+function findHack(arr) {
+  let array = [];
+  let scoreWorth = {
+    A: 30,
+    B: 20,
+    C: 10,
+    D: 5,
+  };
+
+  let scoresCheck = (score) => score === 'A' || score === 'B';
+
+  for (const element of arr) {
+    let sum = 0;
+
+    for (let i = 0; i < element[2].length; i++) {
+      if (scoreWorth[element[2][i]]) {
+        sum += scoreWorth[element[2][i]];
+      }
+    }
+
+    if (element[1] > 200) {
+      array.push(element[0]);
+    } else {
+      if (element[2].length >= 5 && element[2].every(scoresCheck)) {
+        sum += 20;
+      }
+
+      if (element[1] !== sum) {
+        array.push(element[0]);
+      }
+    }
+  }
+  return array;
+}
