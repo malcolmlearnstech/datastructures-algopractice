98f860160d9d19dd7c80b1d0078be0277f9a8579
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-24 15:12:48 -0400

    chore: add initial solution code to js file

--- CodeWars/87-Consonant-Value.js
+++ CodeWars/87-Consonant-Value.js
@@ -1,1 +1,17 @@
-function consonantSolve(s) {}
+function consonantSolve(s) {
+  let alphabet = ' abcdefghijklmnopqrstuvwxyz';
+  let max = 0;
+  s = s.split(/[aeiou]/g);
+  s = s.filter((element) => element !== '');
+
+  s.map((element) => {
+    let sum = 0;
+
+    for (let i = 0; i < element.length; i++) {
+      sum += alphabet.indexOf(element[i]);
+    }
+    max = Math.max(max, sum);
+  });
+
+  return max;
+}
