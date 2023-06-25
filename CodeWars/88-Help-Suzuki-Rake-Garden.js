5add26281f124ae9929fe0b316536946600a5aa4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-25 17:12:14 -0400

    chore: add solution code to js file

--- CodeWars/88-Help-Suzuki-Rake-Garden.js
+++ CodeWars/88-Help-Suzuki-Rake-Garden.js
@@ -1,1 +1,13 @@
-function rakeGarden(garden) {}
+function rakeGarden(garden) {
+  garden = garden.split(' ');
+
+  garden = garden.map((element) => {
+    if (element !== 'gravel' && element !== 'rock') {
+      return 'gravel';
+    } else {
+      return element;
+    }
+  });
+
+  return garden.join(' ');
+}
