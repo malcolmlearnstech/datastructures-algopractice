d3febf5f62865e0ca35e2633118cc62e47785840
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-25 01:00:30 -0400

    chore: add refactored solution code to js file

--- CodeWars/87-Consonant-Value.js
+++ CodeWars/87-Consonant-Value.js
@@ -25,3 +25,43 @@ position in the alphabet. Storing this information in an object, makes accessing
 the corresponding position number a lot easier. It is also less time complex, because 
 we are not using indexOf to iterate thru alphabet string every time to find the value.
 */
+
+function consonantSolve(s) {
+  let alphabet = {
+    b: 2,
+    c: 3,
+    d: 4,
+    f: 6,
+    g: 7,
+    h: 8,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  let max = 0;
+  s = s.split(/[aeiou]/g);
+  s = s.filter((element) => element !== '');
+
+  s.map((element) => {
+    let sum = 0;
+
+    for (let i = 0; i < element.length; i++) {
+      sum += alphabet[element[i]];
+    }
+    max = Math.max(max, sum);
+  });
+
+  return max;
+}
