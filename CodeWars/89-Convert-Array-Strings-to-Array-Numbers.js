3ac88f2bbae9f6a63ae70d8717140e3664da5d26
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-26 22:33:24 -0400

    chore: add solution code to js file

--- CodeWars/89-Convert-Array-Strings-to-Array-Numbers.js
+++ CodeWars/89-Convert-Array-Strings-to-Array-Numbers.js
@@ -1,1 +1,9 @@
-function toNumberArray(stringArray) {}
+function toNumberArray(stringArray) {
+  let newArray = [];
+
+  for (let i = 0; i < stringArray.length; i++) {
+    newArray.push(Number(stringArray[i]));
+  }
+
+  return newArray;
+}
