5f72552fea6654270ce552ae8881a74d9be6463c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-29 23:30:06 -0400

    chore: add solution to js file

--- CodeWars/91-Is-It-A-Palindrome.js
+++ CodeWars/91-Is-It-A-Palindrome.js
@@ -1,1 +1,16 @@
-function isPalindrome(x) {}
+function isPalindrome(x) {
+  let left = 0;
+  let right = x.length - 1;
+
+  x = x.toLowerCase();
+
+  while (left < right) {
+    if (x[left] === x[right]) {
+      left++;
+      right--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
