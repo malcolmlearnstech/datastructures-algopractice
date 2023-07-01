c4d5a4f5cc6d6b898b22a32b4766a40fa3c68a8d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-30 22:05:59 -0400

    chore: add solution code to js file

--- CodeWars/92-Categorize-New-Member.js
+++ CodeWars/92-Categorize-New-Member.js
@@ -1,1 +1,10 @@
-function openOrSenior(data) {}
+function openOrSenior(data) {
+  let categories = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7) {
+      categories.push('Senior');
+    } else categories.push('Open');
+  }
+  return categories;
+}
