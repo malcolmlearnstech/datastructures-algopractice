927958f70e5f1802db3026181416a90b4b13211e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-01 23:06:11 -0400

    chore: add solution code to js file

--- CodeWars/93-Remove-The-Minimum.js
+++ CodeWars/93-Remove-The-Minimum.js
@@ -1,1 +1,13 @@
-function removeSmallest(numbers) {}
+function removeSmallest(numbers) {
+  let copy = [];
+  let smallestNum = Math.min(...numbers);
+  let smallestNumIndex = numbers.indexOf(smallestNum);
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (i !== smallestNumIndex) {
+      copy.push(numbers[i]);
+    }
+  }
+
+  return copy;
+}
