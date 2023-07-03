37ca18be0e0b392454f8694bfab106c29354b4b6
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-02 22:55:10 -0400

    chore: add solution code to js file

--- CodeWars/94-Find-Sum-of-Diagonals.js
+++ CodeWars/94-Find-Sum-of-Diagonals.js
@@ -1,1 +1,9 @@
-function diagonalSum(matrix) {}
+function diagonalSum(matrix) {
+  let diagonal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    diagonal += matrix[i][i];
+  }
+
+  return diagonal;
+}
