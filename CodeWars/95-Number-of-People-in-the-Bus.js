63ea40b9a3cce0d03ee0cd63ea4a6bfd8c02dc15
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-03 23:45:40 -0400

    chore: add solution code to js file

--- CodeWars/95-Number-of-People-in-the-Bus.js
+++ CodeWars/95-Number-of-People-in-the-Bus.js
@@ -1,1 +1,9 @@
-var number = function (busStops) {};
+var number = function (busStops) {
+  let passengers = 0;
+
+  for (let i = 0; i < busStops.length; i++) {
+    passengers += busStops[i][0];
+    passengers -= busStops[i][1];
+  }
+  return passengers;
+};
