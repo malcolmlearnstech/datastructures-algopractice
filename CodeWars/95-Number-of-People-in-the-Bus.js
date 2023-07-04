16798f5168259b059dec164987d0af0685c634e0
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-04 00:00:15 -0400

    chore: add refactored solution code to js file

--- CodeWars/95-Number-of-People-in-the-Bus.js
+++ CodeWars/95-Number-of-People-in-the-Bus.js
@@ -14,3 +14,12 @@ REFACTORED SOLUTION -
 Alternative within the for loop is to just add and subtract 
 the riders that board and depart to passengers variable in one line.
 */
+
+var number = function (busStops) {
+  let passengers = 0;
+
+  for (let i = 0; i < busStops.length; i++) {
+    passengers += busStops[i][0] - busStops[i][1];
+  }
+  return passengers;
+};
