608dc6598c17fc55bfd58acea57feda4cea149ba
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-05 23:32:28 -0400

    chore: add solution code to js file

--- CodeWars/96-Stanton-Measure.js
+++ CodeWars/96-Stanton-Measure.js
@@ -1,1 +1,17 @@
-function stantonMeasure(array) {}
+function stantonMeasure(array) {
+  let n = 0;
+  let nCount = 0;
+
+  array.map((element) => {
+    if (element === 1) {
+      n++;
+    }
+  });
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === n) {
+      nCount++;
+    }
+  }
+  return nCount;
+}
