3c6170fe58dc7ad12bacc6d0ba2d8a15871f4f62
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-05 23:45:56 -0400

    chore: add refactored solution code to js file

--- CodeWars/96-Stanton-Measure.js
+++ CodeWars/96-Stanton-Measure.js
@@ -23,3 +23,15 @@ As an alternative for iterating thru the array and incrementing
 the variable n's value. We use a filter to check for the amount 
 of 1's in the input array then check for length.
 */
+
+function stantonMeasure(array) {
+  let n = array.filter((element) => element === 1).length;
+  let nCount = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === n) {
+      nCount++;
+    }
+  }
+  return nCount;
+}
