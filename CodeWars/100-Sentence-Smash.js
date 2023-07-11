77c70363d5988ded0796ea27cf0233f550f6cc71
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-10 23:39:58 -0400

    chore: add solution code to js file

--- CodeWars/100-Sentence-Smash.js
+++ CodeWars/100-Sentence-Smash.js
@@ -1,1 +1,13 @@
-function smash(words) {}
+function smash(words) {
+  let sentence = '';
+
+  words.map((element, index) => {
+    if (index !== words.length - 1) {
+      sentence += element + ' ';
+    } else {
+      sentence += element;
+    }
+  });
+
+  return sentence;
+}
