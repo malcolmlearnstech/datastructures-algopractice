a582a69029deed56c85fbd38f1a42bb96fe62c33
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-13 23:47:03 -0400

    chore: add refactor explanation and refactored solution code to readme file

--- CodeWars/103-Basic-Mathematical-Operations.js
+++ CodeWars/103-Basic-Mathematical-Operations.js
@@ -9,3 +9,24 @@ function basicOp(operation, value1, value2) {
     return value1 / value2;
   }
 }
+
+/*
+REFACTORED SOLUTION - 
+
+Since we are using a number of if conditionals, a switch 
+statement with the conditions as switch cases can be 
+used alternatively.
+*/
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+  }
+}
