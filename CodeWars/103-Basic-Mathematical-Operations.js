2ecb7bdfe615abf9c90db355754fd3e0a3d5896e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-13 23:41:15 -0400

    chore: add solution code to js file

--- CodeWars/103-Basic-Mathematical-Operations.js
+++ CodeWars/103-Basic-Mathematical-Operations.js
@@ -1,1 +1,11 @@
-function basicOp(operation, value1, value2) {}
+function basicOp(operation, value1, value2) {
+  if (operation === '+') {
+    return value1 + value2;
+  } else if (operation === '-') {
+    return value1 - value2;
+  } else if (operation === '*') {
+    return value1 * value2;
+  } else if (operation === '/') {
+    return value1 / value2;
+  }
+}
