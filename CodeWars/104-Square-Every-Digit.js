f73f4288fe1d06540773853b51b2f12161af6326
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-14 23:50:38 -0400

    chore: add solution code to js file

--- CodeWars/104-Square-Every-Digit.js
+++ CodeWars/104-Square-Every-Digit.js
@@ -1,1 +1,8 @@
-function squareDigits(num) {}
+function squareDigits(num) {
+  num = String(num).split('');
+  let squared = [];
+
+  num.map((element) => squared.push(Math.pow(Number(element), 2)));
+
+  return Number(squared.join(''));
+}
