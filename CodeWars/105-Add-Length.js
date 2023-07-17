3602a6af20b7b533fa4b6f11e9784227c313784a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-16 23:48:02 -0400

    chore: add solution code to js file

--- CodeWars/105-Add-Length.js
+++ CodeWars/105-Add-Length.js
@@ -1,1 +1,10 @@
-function addLength(str) {}
+function addLength(str) {
+  let result = [];
+  str = str.split(' ');
+
+  for (let i = 0; i < str.length; i++) {
+    result.push(`${str[i]} ${str[i].length}`);
+  }
+
+  return result;
+}
