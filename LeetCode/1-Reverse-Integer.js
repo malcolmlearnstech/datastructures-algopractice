eeb19bcea274a441a58b75f89ed075ccb0b0db09
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-18 23:25:07 -0400

    chore: add solution code to js file

--- LeetCode/1-Reverse-Integer.js
+++ LeetCode/1-Reverse-Integer.js
@@ -1,1 +1,42 @@
-var reverse = function (x) {};
+var reverse = function (x) {
+  let result = [];
+
+  if (Math.sign(x) === -1) {
+    x = x * -1;
+    x = String(x).split('');
+
+    for (let i = x.length - 1; i >= 0; i--) {
+      result.push(x[i]);
+    }
+
+    if (result[0] === '0') {
+      result.shift();
+    }
+
+    result = Number(result.join('')) * -1;
+
+    if (result > Math.pow(-2, 31)) {
+      return result;
+    } else {
+      return 0;
+    }
+  } else {
+    x = String(x).split('');
+
+    for (let i = x.length - 1; i >= 0; i--) {
+      result.push(x[i]);
+    }
+
+    if (result[0] === '0') {
+      result.shift();
+    }
+
+    result = Number(result.join(''));
+
+    if (result < Math.pow(2, 31)) {
+      return result;
+    } else {
+      return 0;
+    }
+  }
+};
