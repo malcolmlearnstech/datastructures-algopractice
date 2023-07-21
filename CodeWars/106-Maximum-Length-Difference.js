3a4204f3e9c9b5676081cd6fa49a51457e5f87ef
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-20 22:47:25 -0400

    chore: add solution code to js file

--- CodeWars/106-Maximum-Length-Difference.js
+++ CodeWars/106-Maximum-Length-Difference.js
@@ -1,1 +1,20 @@
-function mxdiflg(a1, a2) {}
+function mxdiflg(a1, a2) {
+  let a1LargestA2Smallest;
+  let a2LargestA1Smallest;
+
+  if (
+    (a1.length === 0 && a2.length === 0) ||
+    a1.length === 0 ||
+    a2.length === 0
+  ) {
+    return -1;
+  } else {
+    a1 = a1.map((element) => element.length);
+    a2 = a2.map((element) => element.length);
+
+    a1LargestA2Smallest = Math.abs(Math.max(...a1) - Math.min(...a2));
+    a2LargestA1Smallest = Math.abs(Math.max(...a2) - Math.min(...a1));
+  }
+
+  return Math.max(a1LargestA2Smallest, a2LargestA1Smallest);
+}
