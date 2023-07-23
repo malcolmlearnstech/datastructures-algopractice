7037a692fd8c0e622be1cc894e564a6afd5ca531
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-22 23:40:52 -0400

    chore: add solution code to js file

--- CodeWars/107-Remove-Duplicates-from-List.js
+++ CodeWars/107-Remove-Duplicates-from-List.js
@@ -1,1 +1,21 @@
-function distinct(a) {}
+function distinct(a) {
+  let result = [];
+  let visited = {};
+
+  for (let i = 0; i < a.length; i++) {
+    if (!visited[a[i]]) {
+      visited[a[i]] = 1;
+    } else {
+      visited[a[i]]++;
+    }
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (visited[a[i]]) {
+      result.push(a[i]);
+      delete visited[a[i]];
+    }
+  }
+
+  return result;
+}
