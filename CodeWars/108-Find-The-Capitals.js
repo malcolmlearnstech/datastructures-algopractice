89837eb33884fc655889cf10924a5018e980f2ff
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-23 23:58:07 -0400

    chore: add solution code to js file

--- CodeWars/108-Find-The-Capitals.js
+++ CodeWars/108-Find-The-Capitals.js
@@ -1,1 +1,12 @@
-var capitals = function (word) {};
+var capitals = function (word) {
+  let result = [];
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for (let i = 0; i < word.length; i++) {
+    if (alphabet.includes(word[i])) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
