34c5643ee2ea358d6fc52294bb04e5a1bafd99ab
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-25 22:27:42 -0400

    chore: add solution code to js file

--- CodeWars/109-Multiplication-Table.js
+++ CodeWars/109-Multiplication-Table.js
@@ -1,1 +1,18 @@
-multiplicationTable = function (size) {};
+multiplicationTable = function (size) {
+  let table = [];
+  let start = 1;
+  let step = 1;
+
+  while (table.length < size) {
+    let row = [];
+
+    while (row.length < size) {
+      row.push(start);
+      start += step;
+    }
+    table.push(row);
+    start = table[table.length - 1][0] + 1;
+    step = start;
+  }
+  return table;
+};
