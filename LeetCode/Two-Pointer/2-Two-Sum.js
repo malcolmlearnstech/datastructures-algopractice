4c2cee5767418c37fd0b7c2b24be121d7146aab4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-26 23:42:00 -0400

    chore: add solution code to js file

--- LeetCode/Two-Pointer/2-Two-Sum.js
+++ LeetCode/Two-Pointer/2-Two-Sum.js
@@ -1,1 +1,16 @@
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let start = 0;
+    while (start < nums.length) {
+      if (i === start) {
+        start++;
+      }
+
+      if (nums[i] + nums[start] === target) {
+        return [i, start];
+      } else start++;
+    }
+  }
+};
