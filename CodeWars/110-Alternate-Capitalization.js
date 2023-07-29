7dfb284c8ba6b49ffd70a8233e4516805fcda486
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-29 18:58:12 -0400

    chore: add solution code to js file

--- CodeWars/110-Alternate-Capitalization.js
+++ CodeWars/110-Alternate-Capitalization.js
@@ -1,1 +1,24 @@
-function capitalize(s) {}
+function capitalize(s) {
+  s = s.split('');
+  let result = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      s[i] = s[i].toUpperCase();
+    }
+  }
+
+  s = s.join('');
+  result.push(s);
+  s = s.toLowerCase().split('');
+
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2) {
+      s[i] = s[i].toUpperCase();
+    }
+  }
+
+  result.push(s.join(''));
+
+  return result;
+}
