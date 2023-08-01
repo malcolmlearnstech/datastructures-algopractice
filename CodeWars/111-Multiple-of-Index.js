de64a8ad51f7d644769614bd3fadf165add329b3
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-07-31 23:57:54 -0400

    chore: add solution code to js file

--- CodeWars/111-Multiple-of-Index.js
+++ CodeWars/111-Multiple-of-Index.js
@@ -1,1 +1,15 @@
-function multipleOfIndex(array) {}
+function multipleOfIndex(array) {
+  let result = [];
+
+  if (array[0] === 0) {
+    result.push(0);
+  }
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] % i === 0) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
+}
