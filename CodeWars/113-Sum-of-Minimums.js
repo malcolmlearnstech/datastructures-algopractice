ffb14ae05d547beb2040c8977fe8ec0f465c3444
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-08-02 23:38:09 -0400

    chore: re-add refactor explanation and code to js file

--- CodeWars/113-Sum-of-Minimums.js
+++ CodeWars/113-Sum-of-Minimums.js
@@ -11,6 +11,14 @@ function sumOfMinimums(arr) {
 /*
 REFACTORED SOLUTION -
 
-We can use map to iterate thru the inner arrays as well, still 
+We can alternatively use map to iterate thru the inner arrays as well, still 
 using Math.min to find the smallest numbers in each.
 */
+
+function sumOfMinimums(arr) {
+  let result = 0;
+
+  arr.map((element) => (result += Math.min(...element)));
+
+  return result;
+}
