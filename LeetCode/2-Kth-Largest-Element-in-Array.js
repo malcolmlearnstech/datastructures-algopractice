7c7940e4a693fb58abf5a5c34418b279c2f3ef18
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-08-05 01:04:05 -0400

    chore: add new solution code w/o sorting

--- LeetCode/2-Kth-Largest-Element-in-Array.js
+++ LeetCode/2-Kth-Largest-Element-in-Array.js
@@ -1,5 +1,31 @@
 var findKthLargest = function (nums, k) {
-  nums = nums.sort((a, b) => b - a);
+  let object = {};
+  let count = 1;
 
-  return nums[k - 1];
+  for (let i = 0; i < nums.length; i++) {
+    if (!object[nums[i]]) {
+      object[nums[i]] = 1;
+    } else {
+      object[nums[i]] += 1;
+    }
+  }
+
+  let numbers = Object.keys(object);
+  let largest = Math.max(...numbers);
+
+  if (count === k) {
+    return largest;
+  }
+
+  while (count < k) {
+    object[largest] -= 1;
+    if (object[largest] === 0) {
+      delete object[largest];
+      numbers = Object.keys(object);
+      largest = Math.max(...numbers);
+    }
+    count++;
+  }
+
+  return largest;
 };
