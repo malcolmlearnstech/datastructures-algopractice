87fc3fa30628a479ea3b9f0c469db61574c56b3f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-08-09 23:11:13 -0400

    chore: add refactored solution code to js file

--- LeetCode/5-Maximum-Count-Positive-Negative-Integer.js
+++ LeetCode/5-Maximum-Count-Positive-Negative-Integer.js
@@ -18,6 +18,38 @@ var maximumCount = function (nums) {
 REFACTORED SOLUTION -
 
 While the original solution has a linear time complexity "O(n)", 
-we can use two while loops to solve this in a slightly less time 
-complex way of "O(log n)"
+we can use two while loops in a two pointer pattern method
+to solve this in a slightly less time complex way of "O(log n)"
 */
+
+var maximumCount = function (nums) {
+  let left = 0;
+  let right = nums.length;
+
+  while (left < right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] > 0) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  const positiveCount = nums.length - left;
+
+  left = 0;
+  right = nums.length;
+
+  while (left < right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    if (nums[mid] >= 0) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  const negativeCount = right;
+
+  return Math.max(positiveCount, negativeCount);
+};
