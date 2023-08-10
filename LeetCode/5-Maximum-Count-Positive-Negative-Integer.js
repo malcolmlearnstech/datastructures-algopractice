f668a0b973d101a75c1545067e4bb60e6a708d07
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-08-09 21:34:09 -0400

    chore: add solution code to js file

--- LeetCode/5-Maximum-Count-Positive-Negative-Integer.js
+++ LeetCode/5-Maximum-Count-Positive-Negative-Integer.js
@@ -1,1 +1,15 @@
-var maximumCount = function (nums) {};
+var maximumCount = function (nums) {
+  let pos = 0;
+  let neg = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (Math.sign(nums[i]) === 1) {
+      pos++;
+    }
+    if (Math.sign(nums[i]) === -1) {
+      neg++;
+    }
+  }
+
+  return Math.max(pos, neg);
+};
