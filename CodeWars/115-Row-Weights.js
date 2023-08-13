1b3dd1dd0e21bbee968696f630d569540b7fedf1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-08-12 23:27:06 -0400

    chore: add solution code to js file

--- CodeWars/115-Row-Weights.js
+++ CodeWars/115-Row-Weights.js
@@ -1,1 +1,14 @@
-function rowWeights(array) {}
+function rowWeights(array) {
+  let first = 0;
+  let second = 0;
+
+  array.map((element, index) => {
+    if (index % 2 === 0) {
+      first += element;
+    } else {
+      second += element;
+    }
+  });
+
+  return [first, second];
+}
