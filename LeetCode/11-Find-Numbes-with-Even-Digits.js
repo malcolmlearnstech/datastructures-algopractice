baf0f0b7bf8593de0d8d84f8d3f47d5ee2c6f8c5
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-08-19 23:49:34 -0400

    chore: add solution code to js file

--- LeetCode/11-Find-Numbes-with-Even-Digits.js
+++ LeetCode/11-Find-Numbes-with-Even-Digits.js
@@ -1,1 +1,13 @@
-var findNumbers = function (nums) {};
+var findNumbers = function (nums) {
+  let evenNums = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let current = nums[i];
+
+    if (String(current).split('').length % 2 === 0) {
+      evenNums++;
+    }
+  }
+
+  return evenNums;
+};
