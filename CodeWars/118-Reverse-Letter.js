8bc63066c3033e59223ad853087d9de72a06a794
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-09-07 23:30:15 -0400

    chore: add refactor explanation to js file

--- CodeWars/118-Reverse-Letter.js
+++ CodeWars/118-Reverse-Letter.js
@@ -7,3 +7,11 @@ function reverseLetter(str) {
 
   return str;
 }
+
+/*
+REFACTORED SOLUTION -
+
+For a less time complex method, we can simply apply replace method 
+to remove any characters that aren't letters, then just use a for 
+loop to read the letters backward
+*/
