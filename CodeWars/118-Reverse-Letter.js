2f73c36f23813dcdca793970075b5875257b8d70
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-09-07 23:30:42 -0400

    chore: add refactored solution code to js file

--- CodeWars/118-Reverse-Letter.js
+++ CodeWars/118-Reverse-Letter.js
@@ -15,3 +15,14 @@ For a less time complex method, we can simply apply replace method
 to remove any characters that aren't letters, then just use a for 
 loop to read the letters backward
 */
+
+function reverseLetter(str) {
+  let reverse = '';
+  str = str.replace(/[^a-z]/g, '');
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+
+  return reverse;
+}
