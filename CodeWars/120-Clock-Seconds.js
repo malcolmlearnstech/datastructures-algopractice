1582dac5714871e330a1271e474a7d570a915f23
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-03 23:41:28 -0400

    chore: add solution code to js file

--- CodeWars/120-Clock-Seconds.js
+++ CodeWars/120-Clock-Seconds.js
@@ -1,1 +1,9 @@
-function past(h, m, s) {}
+function past(h, m, s) {
+  let milliseconds = 0;
+
+  milliseconds += s * 1000;
+  milliseconds += m * 60000;
+  milliseconds += h * 3600000;
+
+  return milliseconds;
+}
