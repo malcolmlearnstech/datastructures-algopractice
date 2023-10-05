c3fe42507d44e0352af948edba097d65a1c4c202
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-04 23:43:08 -0400

    chore: add solution code to js file

--- CodeWars/121-sillyCASE.js
+++ CodeWars/121-sillyCASE.js
@@ -1,1 +1,9 @@
-function sillycase(silly) {}
+function sillycase(silly) {
+  let middle = Math.ceil(silly.length / 2);
+  silly = silly.toLowerCase().split('');
+
+  for (let i = silly.length - 1; i >= middle; i--) {
+    silly[i] = silly[i].toUpperCase();
+  }
+  return silly.join('');
+}
