0f54a75ea2ff6eed5e8272f331cd9be0802cc636
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-05 23:51:00 -0400

    chore: add refactor explanation to js file

--- CodeWars/122-String-Matchup.js
+++ CodeWars/122-String-Matchup.js
@@ -12,3 +12,12 @@ function solve(a, b) {
   });
   return matches;
 }
+
+/*
+REFACTORED SOLUTION -
+
+Alternative to looping thru the "a" array, we can use the filter method 
+to only look at how many elements match the current "b" array element 
+that we are iterating on. This will more than likely still carry the same 
+time complexity as the initial solution.
+*/
