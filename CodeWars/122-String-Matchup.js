1a5bfc25ca7eb54db7da148dac891809b4023ce5
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-05 23:45:48 -0400

    chore: add solution code to js file

--- CodeWars/122-String-Matchup.js
+++ CodeWars/122-String-Matchup.js
@@ -1,1 +1,14 @@
-function solve(a, b) {}
+function solve(a, b) {
+  let matches = [];
+
+  b.forEach((element) => {
+    let match = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (element === a[i]) {
+        match++;
+      }
+    }
+    matches.push(match);
+  });
+  return matches;
+}
