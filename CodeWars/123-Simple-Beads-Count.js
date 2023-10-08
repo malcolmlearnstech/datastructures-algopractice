3edba7213dad58184bab2e1cbb029294e812f82e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-08 02:01:53 -0400

    chore: add refactored solution to js file

--- CodeWars/123-Simple-Beads-Count.js
+++ CodeWars/123-Simple-Beads-Count.js
@@ -16,3 +16,11 @@ REFACTORED SOLUTION -
 After checking that the input is greater than 2, we can use 
 a ternary operator as an alternative to if/else conditional check.
 */
+
+function countRedBeads(n) {
+  if (n < 2) {
+    return 0;
+  }
+
+  return n % 2 ? (n - 1) * 2 : n * 2 - 2;
+}
