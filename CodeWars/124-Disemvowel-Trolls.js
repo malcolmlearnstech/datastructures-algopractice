14e0cb06cefe667bcad3ecc07e97f9a4a5a6fb63
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-09 02:16:00 -0400

    chore: add explanation for second refactored solution

--- CodeWars/124-Disemvowel-Trolls.js
+++ CodeWars/124-Disemvowel-Trolls.js
@@ -28,3 +28,12 @@ vowels to ensure all are caught in the string match method.
 function disemvowel(str) {
   return str.replace(/[aeiouAEIOU]/g, '');
 }
+
+/*
+REFACTORED SOLUTION - 
+
+We can make use of regex in this instance to look for and get rid 
+of any letters that are vowels, employing the global flag. We make 
+sure to include both the capitalized AND lower case version of 
+vowels to ensure all are caught in the string match method.
+*/
