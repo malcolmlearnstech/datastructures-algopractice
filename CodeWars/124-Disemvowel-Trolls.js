05cd356b7ba99f0519bc30c6f28493f0cda031eb
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-08 23:41:50 -0400

    chore: add solution code to js file

--- CodeWars/124-Disemvowel-Trolls.js
+++ CodeWars/124-Disemvowel-Trolls.js
@@ -1,1 +1,17 @@
-function disemvowel(str) {}
+function disemvowel(str) {
+  let newStr = '';
+  let vowels = {
+    a: 1,
+    e: 1,
+    i: 1,
+    o: 1,
+    u: 1,
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    if (!vowels[str[i].toLowerCase()]) {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
