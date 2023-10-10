d446cb69d160c634f23ee27d6a4cf40800b70eee
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-09 23:42:50 -0400

    chore: add solution code to js file

--- CodeWars/125-Sum-of-Array-Averages.js
+++ CodeWars/125-Sum-of-Array-Averages.js
@@ -1,1 +1,13 @@
-const sumAverage = (arr) => {};
+const sumAverage = (arr) => {
+  let averages = 0;
+
+  arr.map((element) => {
+    averages +=
+      element.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      ) / element.length;
+  });
+
+  return Math.floor(averages);
+};
