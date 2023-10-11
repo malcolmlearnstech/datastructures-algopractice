6cd2818d44d883176478ed5aa878c4ee14d0c3c3
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-10 23:18:54 -0400

    chore: add solution code to js file

--- CodeWars/126-Whos-Online.js
+++ CodeWars/126-Whos-Online.js
@@ -1,1 +1,29 @@
-const whosOnline = (friends) => {};
+const whosOnline = (friends) => {
+  let userStatus = {
+    online: [],
+    offline: [],
+    away: [],
+  };
+
+  friends.map((element) => {
+    if (element.status === 'offline') {
+      userStatus.offline.push(element.username);
+    } else if (element.status === 'online' && element.lastActivity > 10) {
+      userStatus.away.push(element.username);
+    } else {
+      userStatus.online.push(element.username);
+    }
+  });
+
+  if (userStatus.online.length === 0) {
+    delete userStatus.online;
+  }
+  if (userStatus.away.length === 0) {
+    delete userStatus.away;
+  }
+  if (userStatus.offline.length === 0) {
+    delete userStatus.offline;
+  }
+
+  return userStatus;
+};
