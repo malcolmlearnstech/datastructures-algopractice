03a72c8bb7821d9c6cba42c09708e03fe4328ad4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-10 23:41:24 -0400

    chore: add refactor explanation to js file

--- CodeWars/126-Whos-Online.js
+++ CodeWars/126-Whos-Online.js
@@ -27,3 +27,13 @@ const whosOnline = (friends) => {
 
   return userStatus;
 };
+
+/*
+REFACTOR SOLUTION -
+
+When mapping thru the array of objects, we can store the user 
+status and user name in variables to avoid repeating such long 
+references to them in their corresponding conditionals. We can 
+also use a for-in loop after populating the return object to check 
+for which status have no names associated with them.
+*/
