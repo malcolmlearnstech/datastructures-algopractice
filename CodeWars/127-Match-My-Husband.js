39d276fcdbf0871f117d5b8ea916cbda4988858e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-11 23:37:11 -0400

    chore: add solution code to js file

--- CodeWars/127-Match-My-Husband.js
+++ CodeWars/127-Match-My-Husband.js
@@ -1,1 +1,14 @@
-function match(usefullness, months) {}
+function match(usefullness, months) {
+  let usefulTotal = 0;
+  let needs = 100;
+
+  for (const element of usefulness) {
+    usefulTotal += element;
+  }
+
+  while (months > 0) {
+    needs = needs - needs * 0.15;
+    months--;
+  }
+  return usefulTotal > needs ? 'Match!' : 'No match!';
+}
