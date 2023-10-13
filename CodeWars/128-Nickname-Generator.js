77ed40d03896d56779157d4144e72d49b5ce31ef
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-12 23:51:44 -0400

    chore: add solution code to js file

--- CodeWars/128-Nickname-Generator.js
+++ CodeWars/128-Nickname-Generator.js
@@ -1,1 +1,9 @@
-function nicknameGenerator(name) {}
+function nicknameGenerator(name) {
+  if (name.length >= 4) {
+    return name[2].match(/[aeiouAEIOU]/g)
+      ? name[0] + name[1] + name[2] + name[3]
+      : name[0] + name[1] + name[2];
+  } else {
+    return 'Error: Name too short';
+  }
+}
