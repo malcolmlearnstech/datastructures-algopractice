66f93c0d8d881e6332e2dff1663c1deee9caffea
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-12 23:59:05 -0400

    chore: add refactored solution code and explanation to js file

--- CodeWars/128-Nickname-Generator.js
+++ CodeWars/128-Nickname-Generator.js
@@ -7,3 +7,19 @@ function nicknameGenerator(name) {
     return 'Error: Name too short';
   }
 }
+
+/*
+REFACTOR SOLUTION -
+
+Alternative to hardcoding the return name, we 
+can use the slice method, and programmatically indicate 
+where the displaying strings should stop.
+*/
+
+function nicknameGenerator(name) {
+  if (name.length < 4) {
+    return 'Error: Name too short';
+  } else {
+    return name[2].match(/[aeiou]/gi) ? name.slice(0, 4) : name.slice(0, 3);
+  }
+}
