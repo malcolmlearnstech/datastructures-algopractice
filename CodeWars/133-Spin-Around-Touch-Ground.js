42b766e7776c5de500c599e6f45d54539781ec46
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-18 23:47:22 -0400

    chore: add solution code to js file

--- CodeWars/133-Spin-Around-Touch-Ground.js
+++ CodeWars/133-Spin-Around-Touch-Ground.js
@@ -1,1 +1,9 @@
-function spinAround(turns) {}
+function spinAround(turns) {
+  let start = 0;
+
+  for (let i = 0; i < turns.length; i++) {
+    turns[i] === 'right' ? (start += 90) : (start -= 90);
+  }
+
+  return start > 0 ? Math.floor(start / 360) : Math.abs(Math.ceil(start / 360));
+}
