8f3e12b86cb2ea96050208f8e71cfb910c403ab2
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-20 20:50:31 -0400

    chore: add solution code to js file

--- CodeWars/134-Capitals-First.js
+++ CodeWars/134-Capitals-First.js
@@ -1,1 +1,19 @@
-function capitalsFirst(str) {}
+function capitalsFirst(str) {
+  let string = [];
+  let alphabet = /[a-zA-z]/g;
+  str = str.split(' ');
+
+  str.map((element) => {
+    if (element[0].match(alphabet) && element[0] === element[0].toUpperCase()) {
+      string.push(element);
+    }
+  });
+
+  str.map((element) => {
+    if (element[0].match(alphabet) && element[0] === element[0].toLowerCase()) {
+      string.push(element);
+    }
+  });
+
+  return string.join(' ');
+}
