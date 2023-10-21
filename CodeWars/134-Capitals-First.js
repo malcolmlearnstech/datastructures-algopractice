6d7b82970aa76ade0d923e7f9112094cd3d7ac9d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-20 20:56:19 -0400

    chore: add refactor explanation to js file

--- CodeWars/134-Capitals-First.js
+++ CodeWars/134-Capitals-First.js
@@ -17,3 +17,15 @@ function capitalsFirst(str) {
 
   return string.join(' ');
 }
+
+/*
+REFACTORED SOLUTION -
+
+Instead of mapping thru the split string twice, and using if conditionals; 
+we can create two separate arrays containing elements beginning with upper 
+case and lower case. Within the conditions of the filter, rather than 
+hardcoding to check for casing, we can apply match and regex to look for 
+matches to either all upper case or lower case letters. With the two arrays 
+exisiting, we can then use Array.concat() to bring the elements together in 
+proper order. Lastly we apply the join method to make the return a string again.
+*/
