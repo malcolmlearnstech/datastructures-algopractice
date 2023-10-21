cf74267cc8d85d383fa76380ae3561e8e18cb354
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-20 22:38:24 -0400

    chore: add solution code to js file

--- CodeWars/135-Jaden-Casing-Strings.js
+++ CodeWars/135-Jaden-Casing-Strings.js
@@ -1,1 +1,10 @@
-String.prototype.toJadenCase = function () {};
+String.prototype.toJadenCase = function () {
+  let string = this.split(' ');
+
+  for (let i = 0; i < string.length; i++) {
+    let separated = string[i].split('');
+    separated[0] = separated[0].toUpperCase();
+    string[i] = separated.join('');
+  }
+  return string.join(' ');
+};
