4a00873c5f99cb3d99b83f3c6f28bc30cd0b31ad
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-20 22:59:54 -0400

    chore: add refactor explanation to js file

--- CodeWars/135-Jaden-Casing-Strings.js
+++ CodeWars/135-Jaden-Casing-Strings.js
@@ -8,3 +8,14 @@ String.prototype.toJadenCase = function () {
   }
   return string.join(' ');
 };
+
+/*
+REFACTORED SOLUTION -
+
+Alternative to the for loop, we can split the string and map 
+through each word element. Rather than doing an additional split 
+on the characters of each work to then capitalize the first 
+letter and then joining, we simply refer to the first letter and 
+capitalize then concatenate a slice of the rest of the word. We 
+then rejoin the whole of the array of words.
+*/
