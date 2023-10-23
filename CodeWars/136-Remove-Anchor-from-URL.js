bf42442361044045dc5bb6387299ea6807f12c2f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-22 23:53:05 -0400

    chore: add solution refactor explanation to js file

--- CodeWars/136-Remove-Anchor-from-URL.js
+++ CodeWars/136-Remove-Anchor-from-URL.js
@@ -4,3 +4,13 @@ function removeUrlAnchor(url) {
 
   return end > 0 ? url.slice(begin, end) : url;
 }
+
+/*
+REFACTORED SOLUTION -
+
+We can use the method of .split() and place a string of the pound 
+sign as the parameter to notate where we want to break up the 
+string. As the string method will return an array, and we want to 
+pull everything before the pound sign, we choose the array reference 
+of zero ("0") which will be the first array element before the pound.
+*/
