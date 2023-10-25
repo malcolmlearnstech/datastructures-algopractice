de9b5384eb25aa8caf6e1a9d92113a8750b62ca1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-24 22:38:06 -0400

    chore: add refactored solution code to js file

--- CodeWars/137-Number-of-Decimal-Digits.js
+++ CodeWars/137-Number-of-Decimal-Digits.js
@@ -8,3 +8,7 @@ REFACTORED SOLUTION -
 We can alternatively use a template literal to convert the 
 number/integer input into a string; then check for length.
 */
+
+function digits(n) {
+  return `${n}`.length;
+}
