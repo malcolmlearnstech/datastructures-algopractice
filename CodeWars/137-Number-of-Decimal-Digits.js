87cf5682e4ee86a2f52f781b5889b6e895f48638
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-24 22:37:41 -0400

    chore: add refactor explanation to js file

--- CodeWars/137-Number-of-Decimal-Digits.js
+++ CodeWars/137-Number-of-Decimal-Digits.js
@@ -1,3 +1,10 @@
 function digits(n) {
   n.toString().split('').length;
 }
+
+/*
+REFACTORED SOLUTION -
+
+We can alternatively use a template literal to convert the 
+number/integer input into a string; then check for length.
+*/
