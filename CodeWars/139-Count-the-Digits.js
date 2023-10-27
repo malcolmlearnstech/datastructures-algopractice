cf3b749162c054c448f981024147dbbabdc4999b
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-26 22:22:06 -0400

    chore: add solution code to js file

--- CodeWars/139-Count-the-Digits.js
+++ CodeWars/139-Count-the-Digits.js
@@ -1,1 +1,14 @@
-function nbDig(n, d) {}
+function nbDig(n, d) {
+  let count = 0;
+
+  for (let i = 0; i <= n; i++) {
+    let current = String(i * i);
+
+    for (let j = 0; j < current.length; j++) {
+      if (Number(current[j]) === d) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
