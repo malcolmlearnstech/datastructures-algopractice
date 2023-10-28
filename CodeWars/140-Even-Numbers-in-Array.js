585238299fe8cadf850f3caee398ea038eba8548
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-27 22:48:46 -0400

    chore: add solution code to js file

--- CodeWars/140-Even-Numbers-in-Array.js
+++ CodeWars/140-Even-Numbers-in-Array.js
@@ -1,1 +1,11 @@
-function evenNumbers(array, number) {}
+function evenNumbers(array, number) {
+  let evens = [];
+
+  for (let i = array.length - 1; evens.length < number; i--) {
+    if (array[i] % 2 === 0) {
+      evens.push(array[i]);
+    }
+  }
+
+  return evens.reverse();
+}
