a7367be7b776060a85968f739ed7c1c4e53dce35
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-27 22:52:06 -0400

    chore: add refactor explanation to js file

--- CodeWars/140-Even-Numbers-in-Array.js
+++ CodeWars/140-Even-Numbers-in-Array.js
@@ -9,3 +9,14 @@ function evenNumbers(array, number) {
 
   return evens.reverse();
 }
+
+/*
+REFACTORED SOLUTION -
+
+We can initially apply the filter method to narrow down the 
+input array to only the even numbers. Then with new information 
+learned from MDN, use the slice method entering a negative form 
+of the "number" input as the perimeter to indicate how far 
+backwards from the end we should pick even numbers out to return 
+as our output array.
+*/
