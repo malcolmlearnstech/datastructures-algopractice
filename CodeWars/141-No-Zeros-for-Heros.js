38a6aaab740dfc3738eab0443c1501b94a6ed208
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-28 23:05:59 -0400

    chore: add solution code to js file

--- CodeWars/141-No-Zeros-for-Heros.js
+++ CodeWars/141-No-Zeros-for-Heros.js
@@ -1,1 +1,9 @@
-function noBoringZeros(n) {}
+function noBoringZeros(n) {
+  n = String(n).split('');
+
+  for (let i = n.length - 1; n[i] === '0'; i--) {
+    n[i] = '';
+  }
+
+  return Number(n.join(''));
+}
