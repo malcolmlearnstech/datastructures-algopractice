91942f770d5ab6f2246cdccbe93d5a373f80cce9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-29 23:46:07 -0400

    chore: add refactored solution code to js file

--- CodeWars/142-How-Many-Unique-Consonants.js
+++ CodeWars/142-How-Many-Unique-Consonants.js
@@ -17,3 +17,16 @@ Within the regex paramter used in the match method, alternative
 to listing all consonants, we can use "0-9" to exclude numbers 
 and "\W" to exclude any special characters or symbols.
 */
+
+function countConsonants(str) {
+  let cons = {};
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(/[^aeiou0-9\W]/g) && !cons[str[i]]) {
+      cons[str[i]] = 1;
+    }
+  }
+
+  return Object.keys(cons).length;
+}
