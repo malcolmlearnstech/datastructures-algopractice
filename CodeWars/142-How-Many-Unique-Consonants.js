176b2397c5c7a9ccf12ec39ad3ff101578f63bb0
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-29 23:29:04 -0400

    chore: add solution code to js file

--- CodeWars/142-How-Many-Unique-Consonants.js
+++ CodeWars/142-How-Many-Unique-Consonants.js
@@ -1,1 +1,11 @@
-function countConsonants(str) {}
+function countConsonants(str) {
+  let cons = {};
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].match(/[bcdfghjklmnpqrstvwxyz]/g) && !cons[str[i]]) {
+      cons[str[i]] = 1;
+    }
+  }
+  return Object.keys(cons).length;
+}
