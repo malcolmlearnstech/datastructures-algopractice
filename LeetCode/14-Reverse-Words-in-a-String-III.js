7593bb16a3ed3b8a48b36cd7c8fb7c061d9769b3
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-10-31 23:47:43 -0400

    chore: add solution code to js file

--- LeetCode/14-Reverse-Words-in-a-String-III.js
+++ LeetCode/14-Reverse-Words-in-a-String-III.js
@@ -1,1 +1,10 @@
-var reverseWords = function (s) {};
+var reverseWords = function (s) {
+  let flipped = [];
+
+  s = s.split(' ');
+  s.map((element) => {
+    flipped.push(element.split('').reverse().join(''));
+  });
+
+  return flipped.join(' ');
+};
