7e2dd110db7cda30141f846de388c110f6d9b397
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-01 23:03:44 -0400

    chore: add solution code to js file

--- CodeWars/144-Flatten-and-Sort-Array.js
+++ CodeWars/144-Flatten-and-Sort-Array.js
@@ -1,1 +1,13 @@
-function flattenAndSort(array) {}
+function flattenAndSort(array) {
+  let resultArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      array[i].map((element) => {
+        resultArray.push(element);
+      });
+    }
+  }
+
+  return resultArray.sort((a, b) => a - b);
+}
