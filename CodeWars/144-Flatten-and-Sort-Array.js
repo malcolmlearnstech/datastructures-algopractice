8d25f3ff84b7fb4fbecd6b2ee52b184759b97f94
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-01 23:28:12 -0400

    chore: add refactored solution code to js file

--- CodeWars/144-Flatten-and-Sort-Array.js
+++ CodeWars/144-Flatten-and-Sort-Array.js
@@ -21,3 +21,15 @@ two-dimensional array, we can elect to push a shallow copy
 of the current array element into our resultArray to later 
 have the integers sorted.
 */
+
+function flattenAndSort(array) {
+  let resultArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      resultArray.push(...array[i]);
+    }
+  }
+
+  return resultArray.sort((a, b) => a - b);
+}
