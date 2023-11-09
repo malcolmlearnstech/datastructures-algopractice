22fbc175876f57e91971193a71863f59dda3a3e6
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-08 22:49:01 -0500

    chore: add solution code to js file

--- CodeWars/148-Fix-String-Case.js
+++ CodeWars/148-Fix-String-Case.js
@@ -1,1 +1,16 @@
-function solve(s) {}
+function solve(s) {
+  let lowers = 0;
+  let uppers = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].match(/[a-z]/g)) {
+      lowers++;
+    } else uppers++;
+  }
+
+  if (lowers > uppers) {
+    return s.toLowerCase();
+  } else if (uppers > lowers) {
+    return s.toUpperCase();
+  } else return s.toLowerCase();
+}
