e3c3a4e90ed57a1d9d5ec67a2e258a1ca237700c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-09 23:40:42 -0500

    chore: add solution code to js file

--- CodeWars/149-Summing-Number-Digits.js
+++ CodeWars/149-Summing-Number-Digits.js
@@ -1,1 +1,11 @@
-function sumDigits(number) {}
+function sumDigits(number) {
+  let sum = 0;
+
+  let array = Math.abs(number)
+    .toString()
+    .split('')
+    .map((element) => Number(element));
+  array.map((element) => (sum += element));
+
+  return sum;
+}
