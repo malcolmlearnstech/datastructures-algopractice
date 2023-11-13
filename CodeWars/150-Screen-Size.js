cf4007b77b29cb34fdb5cdb48c6919ee5b9edfb9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-12 23:16:16 -0500

    chore: add refactored solution code to js file

--- CodeWars/150-Screen-Size.js
+++ CodeWars/150-Screen-Size.js
@@ -16,3 +16,9 @@ calculated numbers to the created variable string, we can
 set the function up to return a template literal. This 
 will also decrease our space complexity.
 */
+
+function findScreenHeight(width, ratio) {
+  ratio = ratio.split(':');
+
+  return `${width}x${(width * Number(ratio[1])) / Number(ratio[0])}`;
+}
