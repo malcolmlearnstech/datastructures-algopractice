d43160f82f11969e119b3708e742e81b79dd48a1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-15 13:09:48 -0500

    chore: add solution code to js file

--- CodeWars/152-Longest-Vowel-Chain.js
+++ CodeWars/152-Longest-Vowel-Chain.js
@@ -1,1 +1,16 @@
-function solve(s) {}
+function solve(s) {
+  let max = 0;
+  let rackUp = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char.match(/[aeiou]/g)) {
+      rackUp++;
+    } else {
+      max = Math.max(max, rackUp);
+      rackUp = 0;
+    }
+  }
+
+  return max;
+}
