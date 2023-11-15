6fb7937237f5f331be380d451c4f1af4f032fe8d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-15 00:38:28 -0500

    chore: add refactor explanation to js file

--- CodeWars/151-Numbers-In-Strings.js
+++ CodeWars/151-Numbers-In-Strings.js
@@ -10,3 +10,12 @@ function solve(s) {
 
   return max;
 }
+
+/*
+REFACTOR SOLUTION -
+
+Because of using regex for all letters in the split 
+returns some empty strings, we can employ filter to 
+ensure that we are mapping thru an array of only number 
+strings.
+*/
