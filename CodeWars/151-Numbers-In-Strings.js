fce28e7a5b08b38bc08342f604efea279b444a11
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-14 23:56:16 -0500

    chore: add solution code to js file

--- CodeWars/151-Numbers-In-Strings.js
+++ CodeWars/151-Numbers-In-Strings.js
@@ -1,1 +1,12 @@
-function solve(s) {}
+function solve(s) {
+  let letters = /[a-z]/g;
+  let max = 0;
+
+  s.split(letters).map((element) => {
+    if (element !== null && Number(element) > max) {
+      max = Number(element);
+    }
+  });
+
+  return max;
+}
