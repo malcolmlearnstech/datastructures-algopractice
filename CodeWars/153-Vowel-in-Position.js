eca98c46139163a4bd58c76a7568368a97b922ac
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-16 23:41:31 -0500

    chore: add refactored solution code to js file

--- CodeWars/153-Vowel-in-Position.js
+++ CodeWars/153-Vowel-in-Position.js
@@ -12,3 +12,7 @@ Alternative to match method, we can type out all vowels
 (upper & lower case) in a string and use the includes 
 method instead.
 */
+
+function checkVowel(string, position) {
+  return 'aeiouAEIOU'.includes(string[position]) ? true : false;
+}
