13f07d357a769a6d93f52c8b7defdfab1ce8ac3a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-18 23:42:38 -0500

    chore: add solution code to js file

--- CodeWars/154-String-Scramble.js
+++ CodeWars/154-String-Scramble.js
@@ -1,1 +1,7 @@
-function scramble(str, arr) {}
+function scramble(str, arr) {
+  let string = {};
+
+  arr.map((element, index) => (string[element] = str[index]));
+
+  return Object.values(string).join('');
+}
