c4b18d0b4e222feda5fd3a596d941077be670618
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-19 23:36:53 -0500

    chore: add solution code to js file

--- CodeWars/155-Cut-Array-Smaller.js
+++ CodeWars/155-Cut-Array-Smaller.js
@@ -1,1 +1,19 @@
-function makeParts(arr, chunkSize) {}
+function makeParts(arr, chunkSize) {
+  let array = [];
+  let subarray = [];
+
+  if (chunkSize > arr.length) {
+    return [arr];
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (subarray.length < chunkSize) {
+        subarray.push(arr[i]);
+      } else {
+        array.push(subarray);
+        subarray = [arr[i]];
+      }
+    }
+    array.push(subarray);
+  }
+  return array;
+}
