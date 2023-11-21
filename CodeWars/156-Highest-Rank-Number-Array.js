67bfdac81e939467c5303f5e945eda5b8177dd9f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-20 20:00:56 -0500

    chore: add solution code to js file

--- CodeWars/156-Highest-Rank-Number-Array.js
+++ CodeWars/156-Highest-Rank-Number-Array.js
@@ -1,1 +1,22 @@
-function highestRank(arr) {}
+function highestRank(arr) {
+  let frequency = {};
+  let max;
+  let highestCount = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (!frequency[element]) {
+      frequency[element] = 1;
+    } else frequency[element]++;
+  }
+
+  max = Math.max(...Object.values(frequency));
+
+  for (const key in frequency) {
+    if (frequency[key] === max) {
+      highestCount.push(Number(key));
+    }
+  }
+
+  return highestCount.length <= 1 ? highestCount[0] : Math.max(...highestCount);
+}
