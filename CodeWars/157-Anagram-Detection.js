99b0bae90560d3e5cbf09dd5051a1714bd76c7e3
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-22 23:50:59 -0500

    chore: add solution code to js file

--- CodeWars/157-Anagram-Detection.js
+++ CodeWars/157-Anagram-Detection.js
@@ -1,1 +1,27 @@
-var isAnagram = function (test, original) {};
+var isAnagram = function (test, original) {
+  let testCount = {};
+  let originalCount = {};
+  test = test.toLowerCase();
+  original = original.toLowerCase();
+
+  if (test.length === original.length) {
+    for (let i = 0; i < test.length; i++) {
+      if (!testCount[test[i]]) {
+        testCount[test[i]] = 1;
+      } else testCount[test[i]]++;
+    }
+
+    for (let i = 0; i < original.length; i++) {
+      if (!originalCount[original[i]]) {
+        originalCount[original[i]] = 1;
+      } else originalCount[original[i]]++;
+    }
+  } else return false;
+
+  for (const key in testCount) {
+    if (originalCount[key] !== testCount[key]) {
+      return false;
+    }
+  }
+  return true;
+};
