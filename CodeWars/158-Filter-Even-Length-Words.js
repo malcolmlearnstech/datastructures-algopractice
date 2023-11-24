4cf86c2a121aaf90d84f7327767d04a0ea99848c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-23 23:53:34 -0500

    chore: add solution code to js file

--- CodeWars/158-Filter-Even-Length-Words.js
+++ CodeWars/158-Filter-Even-Length-Words.js
@@ -1,1 +1,10 @@
-function filterEvenLengthWords(words) {}
+function filterEvenLengthWords(words) {
+  let array = [];
+
+  words.map((element) => {
+    if (element.length % 2 === 0) {
+      array.push(element);
+    }
+  });
+  return array;
+}
