d6b264f29d6c1d77d08453e07c4f88f063fbaf34
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-24 00:26:29 -0500

    chore: add refactored solution code to js file

--- CodeWars/158-Filter-Even-Length-Words.js
+++ CodeWars/158-Filter-Even-Length-Words.js
@@ -17,3 +17,7 @@ space complexity, we can make use of the filter method in
 our return; while using the given condition from the problem 
 as our filtering rule.
 */
+
+function filterEvenLengthWords(words) {
+  return words.filter((element) => element.length % 2 === 0);
+}
