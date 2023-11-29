94fc0bc11d7329ce4121ceb1816b0d4fdda03175
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-29 18:42:55 -0500

    chore: add refactor explanation to js file

--- CodeWars/161-String-Reverse-Slicing.js
+++ CodeWars/161-String-Reverse-Slicing.js
@@ -8,3 +8,17 @@ function reverseSlice(str) {
   }
   return array;
 }
+
+/*
+
+REFACTORED SOLUTION - 
+
+In this alternative solution, we can pregenerate an array of 
+input length, which will allow us to map without making an array 
+of our original input. While mapping thru the pre-generated array, 
+we can employ index in use of slice to return a string with each 
+further character taken off. We'll be able to take off the correct 
+number of characters from the front because we have access to the 
+"index" paramter from our mapping thru the generated array of input 
+"str"s length.
+*/
