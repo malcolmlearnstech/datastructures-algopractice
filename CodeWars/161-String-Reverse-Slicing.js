6dcb1126a5824a93e88552459fd586eff45745ed
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-28 23:54:07 -0500

    chore: add solution code to js file

--- CodeWars/161-String-Reverse-Slicing.js
+++ CodeWars/161-String-Reverse-Slicing.js
@@ -1,1 +1,10 @@
-function reverseSlice(str) {}
+function reverseSlice(str) {
+  let array = [];
+  str = str.split('').reverse();
+
+  while (str.length > 0) {
+    array.push(str.join(''));
+    str.shift();
+  }
+  return array;
+}
