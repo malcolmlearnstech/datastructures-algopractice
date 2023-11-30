9a796d662eea30837e157601a223e7a766a92222
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-29 19:20:48 -0500

    chore: add solution code to js file

--- CodeWars/162-Fruit-String-Calculator.js
+++ CodeWars/162-Fruit-String-Calculator.js
@@ -1,1 +1,12 @@
-function calculate(string) {}
+function calculate(string) {
+  string = string.split(' ');
+  let nums = string
+    .filter((element) => element.match(/[\d]/g))
+    .map((element) => (element = Number(element)));
+
+  if (string.includes('gains')) {
+    return nums[0] + nums[1];
+  } else if (string.includes('loses')) {
+    return nums[0] - nums[1];
+  }
+}
