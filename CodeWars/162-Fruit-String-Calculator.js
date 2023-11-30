bd4a657a713269c241d5d746d2f112553893ae00
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-29 23:24:14 -0500

    chore: add refactor explanation to js file

--- CodeWars/162-Fruit-String-Calculator.js
+++ CodeWars/162-Fruit-String-Calculator.js
@@ -10,3 +10,14 @@ function calculate(string) {
     return nums[0] - nums[1];
   }
 }
+
+/*
+REFACTORED SOLUTION - 
+
+After being exposed to the "eval" function, we see 
+that we can alternatively keep the numbers as strings 
+rather than updating them into numbers (like our map 
+dictates). We can then declare a "math" variable then 
+place all elements of our mathematic string in a template 
+literally with the "eval" function wrapped around it.
+*/
