8274e1621d518d7960d1cc49fde8bd6f46ec6325
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-30 21:51:37 -0500

    chore: add refactor explanation to js file

--- CodeWars/163-String-Transformer.js
+++ CodeWars/163-String-Transformer.js
@@ -14,3 +14,12 @@ function stringTransformer(str) {
     )
     .join(' ');
 }
+
+/*
+REFACTORED SOLUTION - 
+
+Rather than employing the map method twice, we can 
+switch the order of the words, split the characters 
+up and then use map once to go over the characters. 
+Then finally employ the join method once at the end.
+*/
