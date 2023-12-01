8239d3105e333cfc92a0a0d5fec3eda1acd29929
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-30 21:52:07 -0500

    chore: add refactored solution code to js file

--- CodeWars/163-String-Transformer.js
+++ CodeWars/163-String-Transformer.js
@@ -23,3 +23,15 @@ switch the order of the words, split the characters
 up and then use map once to go over the characters. 
 Then finally employ the join method once at the end.
 */
+
+function stringTransformer(str) {
+  str = str.split(' ').reverse().join(' ').split('');
+
+  return str
+    .map((element) =>
+      element === element.toUpperCase()
+        ? element.toLowerCase()
+        : element.toUpperCase()
+    )
+    .join('');
+}
