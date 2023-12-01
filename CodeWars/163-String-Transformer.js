bf47e5401a90636c54cf7992ffc19902cf7261a4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-30 21:48:18 -0500

    chore: add solution code to js file

--- CodeWars/163-String-Transformer.js
+++ CodeWars/163-String-Transformer.js
@@ -1,1 +1,16 @@
-function stringTransformer(str) {}
+function stringTransformer(str) {
+  str = str.split(' ').reverse();
+
+  return str
+    .map((element) =>
+      element
+        .split('')
+        .map((item) => {
+          return item === item.toUpperCase()
+            ? item.toLowerCase()
+            : item.toUpperCase();
+        })
+        .join('')
+    )
+    .join(' ');
+}
