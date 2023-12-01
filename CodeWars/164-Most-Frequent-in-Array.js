007660b0e98e63d286d42061befca6a4e9279993
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-11-30 23:44:08 -0500

    chore: add solution code to js file

--- CodeWars/164-Most-Frequent-in-Array.js
+++ CodeWars/164-Most-Frequent-in-Array.js
@@ -1,1 +1,15 @@
-function mostFrequentItemCount(collection) {}
+function mostFrequentItemCount(collection) {
+  let object = {};
+
+  if (collection.length == 0) {
+    return 0;
+  } else {
+    collection.map((element) => {
+      if (!object[element]) {
+        object[element] = 1;
+      } else object[element]++;
+    });
+  }
+
+  return Math.max(...Object.values(object));
+}
