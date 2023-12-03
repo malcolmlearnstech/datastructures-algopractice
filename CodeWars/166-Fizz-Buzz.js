8253cbc702836093ec79a2959ef42c73eb6e7102
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-02 20:20:51 -0500

    chore: add solution code to js file

--- CodeWars/166-Fizz-Buzz.js
+++ CodeWars/166-Fizz-Buzz.js
@@ -1,1 +1,13 @@
-function fizzbuzz(n) {}
+function fizzbuzz(n) {
+  let array = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      array.push('Buzz');
+    } else if (i % 3 === 0) {
+      array.push('Fizz');
+    } else array.push(i);
+  }
+  return array;
+}
