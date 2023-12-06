9824efdff51d0508d8b2efc3166fd344715e3d90
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-05 23:25:44 -0500

    chore: add solution code to js file

--- CodeWars/168-Sum-of-Array-Singles.js
+++ CodeWars/168-Sum-of-Array-Singles.js
@@ -1,1 +1,16 @@
-function repeats(arr) {}
+function repeats(arr) {
+  let freq = {};
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!freq[arr[i]]) {
+      freq[arr[i]] = 1;
+    } else freq[arr[i]]++;
+  }
+
+  for (const element in freq) {
+    if (freq[element] === 1) {
+      num += Number(element);
+    }
+  }
+  return num;
+}
