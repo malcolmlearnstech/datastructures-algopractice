7d94752e191a034003d14c4cb494750255bd4323
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-06 23:58:58 -0500

    chore: add refactor explanation to js file

--- CodeWars/169-Smallest-Value-of-an-Array.js
+++ CodeWars/169-Smallest-Value-of-an-Array.js
@@ -3,3 +3,12 @@ function min(arr, toReturn) {
 
   return toReturn === 'value' ? Math.min(...arr) : index;
 }
+
+/*
+
+REFACTORED SOLUTION -
+
+If you want to shorten the lines mainly, just 
+put what is stored in the "index" variable into 
+the ternary operator.
+*/
