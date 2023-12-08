4c85dcf2e08aaefc153e4723a3cc2a1a1b9b40fb
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-07 23:39:26 -0500

    chore: add solution code to js file

--- CodeWars/170-Alphabetical-Sequence.js
+++ CodeWars/170-Alphabetical-Sequence.js
@@ -1,1 +1,17 @@
-function alphaSeq(str) {}
+function alphaSeq(str) {
+  str = str.toLowerCase().split('').sort();
+
+  let alphabet = ' abcdefghijklmnopqrstuvwxyz';
+  let comeback = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let string = '';
+    string += str[i].toUpperCase();
+    while (string.length < alphabet.indexOf(str[i])) {
+      string += str[i];
+    }
+    comeback.push(string);
+  }
+
+  return comeback.join(',');
+}
