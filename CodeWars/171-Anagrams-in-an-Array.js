1f4b895f3897c0db66846f4c901a812ef810b7df
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-08 22:57:08 -0500

    chore: add solution code to js file

--- CodeWars/171-Anagrams-in-an-Array.js
+++ CodeWars/171-Anagrams-in-an-Array.js
@@ -1,1 +1,23 @@
-function anagramCounter(wordsArray) {}
+function anagramCounter(wordsArray) {
+  wordsArray = wordsArray.map((element) =>
+    element.toLowerCase().split('').sort().join('')
+  );
+  let anagrams = 0;
+  let left = 0;
+
+  while (left < wordsArray.length - 1) {
+    let right = left + 1;
+
+    while (right < wordsArray.length) {
+      let leftWord = wordsArray[left];
+      let rightWord = wordsArray[right];
+
+      if (leftWord === rightWord) {
+        anagrams++;
+      }
+      right++;
+    }
+    left++;
+  }
+  return anagrams;
+}
