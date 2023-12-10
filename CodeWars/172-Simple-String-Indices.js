2af380e588b4f7fe8476494f67af91fc5e4a77eb
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-09 23:25:30 -0500

    chore: add solution code to js file

--- CodeWars/172-Simple-String-Indices.js
+++ CodeWars/172-Simple-String-Indices.js
@@ -1,1 +1,19 @@
-function solve(str, idx) {}
+function solve(str, idx) {
+  if (str[idx] !== '(') {
+    return -1;
+  }
+
+  let notation = 0;
+
+  for (let i = idx; i < str.length; i++) {
+    if (str[i] === '(') {
+      notation++;
+    }
+    if (str[i] === ')') {
+      notation--;
+    }
+    if (notation === 0) {
+      return i;
+    }
+  }
+}
