df2564b5c08cb86ea6537e60c4900d9903ed317c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-10 23:53:54 -0500

    chore: add solution code to js file

--- CodeWars/174-Reordering.js
+++ CodeWars/174-Reordering.js
@@ -1,1 +1,15 @@
-function reOrdering(text) {}
+function reOrdering(text) {
+  let newOrder = [];
+  text = text.split(' ');
+
+  newOrder.push(
+    text.filter((element) => element[0] === element[0].toUpperCase())[0]
+  );
+  text.map((element) => {
+    if (element[0] !== element[0].toUpperCase()) {
+      newOrder.push(element);
+    }
+  });
+
+  return newOrder.join(' ');
+}
