36b1be3e28f3355dd668c76297e0719cfb7347ea
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-10 23:59:40 -0500

    chore: add refactor explanation to js file

--- CodeWars/174-Reordering.js
+++ CodeWars/174-Reordering.js
@@ -13,3 +13,15 @@ function reOrdering(text) {
 
   return newOrder.join(' ');
 }
+
+/*
+REFACTORED SOLUTION -
+
+Instead pushing the elements by condition into an array, we 
+can create variables to hold the elements matching both 
+conditions (word beginning with a capital letter and words 
+that do not). At the end of the function we return a shallow 
+copy of both arrays, which keep them from being two 
+dimensional, then apply the join method, as all elements will 
+be held in a single dimensional array.
+*/
