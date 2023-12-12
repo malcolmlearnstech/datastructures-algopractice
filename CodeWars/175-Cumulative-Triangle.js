51baeb8eb75a7e57301798901599cada6f047e11
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-11 23:48:21 -0500

    chore: add solution code to js file

--- CodeWars/175-Cumulative-Triangle.js
+++ CodeWars/175-Cumulative-Triangle.js
@@ -1,1 +1,17 @@
-function cumulativeTriangle(n) {}
+function cumulativeTriangle(n) {
+  let start = 1;
+  let increment = 0;
+  let row = [];
+
+  while (increment < n) {
+    start += increment;
+    increment++;
+  }
+
+  for (let i = 0; i < n; i++) {
+    row.push(start);
+    start++;
+  }
+
+  return row.reduce((a, b) => a + b, 0);
+}
