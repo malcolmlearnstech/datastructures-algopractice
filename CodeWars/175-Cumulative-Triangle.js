f043d91f91e93cd9084fe42f42d68f1758895020
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-11 23:52:55 -0500

    chore: add refactor explanation to js file

--- CodeWars/175-Cumulative-Triangle.js
+++ CodeWars/175-Cumulative-Triangle.js
@@ -15,3 +15,12 @@ function cumulativeTriangle(n) {
 
   return row.reduce((a, b) => a + b, 0);
 }
+
+/*
+REFACTORED SOLUTION -
+
+Alternative to building out the row in an array and 
+run the reduce method, we can initialize rowTotal as 
+a 0 number, then use the second for loop to just 
+increment to the rowTotal for returning at the end.
+*/
