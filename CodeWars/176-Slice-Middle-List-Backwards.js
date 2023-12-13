d2e6e7e65fbd80fed80c198d8ddd64c94333f4b0
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-12 23:40:50 -0500

    chore: add refactor explanation to js file

--- CodeWars/176-Slice-Middle-List-Backwards.js
+++ CodeWars/176-Slice-Middle-List-Backwards.js
@@ -4,3 +4,15 @@ function reverseMiddle(array) {
 
   return frontRemoved.reverse();
 }
+
+/*
+REFACTORED SOLUTION -
+
+Rather than the complicated process of figuring out how to 
+account for what's left over of the array versus how to 
+manipulate the array, we can create a for loop. For each 
+iteration of the loop, using the same calculation from the 
+intitial solution, we can set up the array to remove one 
+element from the front and one element from the end of the 
+input array.
+*/
