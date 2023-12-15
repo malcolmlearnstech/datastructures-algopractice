b2fba3073f911fa7e06cd4f406ed108f2746c8c9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-14 23:47:50 -0500

    chore: add solution code to js file

--- CodeWars/179-Form-the-Minimum.js
+++ CodeWars/179-Form-the-Minimum.js
@@ -1,1 +1,9 @@
-function minValue(values) {}
+function minValue(values) {
+  let digits = [];
+  values.map((element) => {
+    if (!digits.includes(element)) {
+      digits.push(element);
+    }
+  });
+  return Number(digits.sort((a, b) => a - b).join(''));
+}
