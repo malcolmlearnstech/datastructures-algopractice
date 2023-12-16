0409dca5f74a08fd38ec273692863c0604524ac1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-15 23:56:48 -0500

    chore: add solution code to js file

--- CodeWars/180-Lario-and-Muigi.js
+++ CodeWars/180-Lario-and-Muigi.js
@@ -1,1 +1,14 @@
-function pipeFix(numbers) {}
+function pipeFix(numbers) {
+  let i = 1;
+
+  while (i < numbers.length) {
+    let current = numbers[i];
+
+    if (current - 1 !== numbers[i - 1]) {
+      numbers.splice(i, 0, numbers[i - 1] + 1);
+    } else {
+      i++;
+    }
+  }
+  return numbers;
+}
