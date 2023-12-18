a56b1b56f71b5f66c9d9e03a8b1bdb452c840e9a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-17 23:44:22 -0500

    chore: add solution code to js file

--- CodeWars/182-Encrypt-This.js
+++ CodeWars/182-Encrypt-This.js
@@ -1,1 +1,17 @@
-var encryptThis = function (text) {};
+var encryptThis = function (text) {
+  text = text.split(' ');
+  let encrypted = [];
+
+  text.forEach((word) => {
+    let temp;
+    word = word.split('');
+    word[0] = word.join('').charCodeAt(0);
+    if (word.length > 1) {
+      temp = word[word.length - 1];
+      word[word.length - 1] = word[1];
+      word[1] = temp;
+    }
+    encrypted.push(word.join(''));
+  });
+  return encrypted.join(' ');
+};
