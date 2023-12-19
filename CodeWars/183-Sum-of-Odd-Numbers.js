a92456ddd529e1e0cd0c2bdd8d446294b88406a2
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-18 23:14:26 -0500

    chore: add solution code to js file

--- CodeWars/183-Sum-of-Odd-Numbers.js
+++ CodeWars/183-Sum-of-Odd-Numbers.js
@@ -1,1 +1,16 @@
-function rowSumOddNumbers(n) {}
+function rowSumOddNumbers(n) {
+  let rowStep = 2;
+  let rowStart = 1;
+  let sum = 0;
+
+  for (let i = 1; i < n; i++) {
+    rowStart += rowStep;
+    rowStep += 2;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    sum += rowStart;
+    rowStart += 2;
+  }
+  return sum;
+}
