6e67ba476ff3108ea2a36e6effaa2ffe26dcf2a1
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-19 23:56:37 -0500

    chore: add solution code to js file

--- CodeWars/184-Largest-Pair-Sum.js
+++ CodeWars/184-Largest-Pair-Sum.js
@@ -1,1 +1,8 @@
-function largestPairSum(numbers) {}
+function largestPairSum(numbers) {
+  let firstLargest = Math.max(...numbers);
+  numbers.splice(numbers.indexOf(firstLargest), 1);
+
+  let secondLargest = Math.max(...numbers);
+
+  return firstLargest + secondLargest;
+}
