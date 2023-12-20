ea036e7f858f4c8c934381d5151a334fd96fb5dd
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-19 23:58:09 -0500

    chore: add refactored solution code to js file

--- CodeWars/184-Largest-Pair-Sum.js
+++ CodeWars/184-Largest-Pair-Sum.js
@@ -16,3 +16,9 @@ elements from the array, we sort the original array instead.
 After sorting in descending order, we can just return the first 
 two elements of the adjusted array.
 */
+
+function largestPairSum(numbers) {
+  numbers = numbers.sort((a, b) => b - a);
+
+  return numbers[0] + numbers[1];
+}
