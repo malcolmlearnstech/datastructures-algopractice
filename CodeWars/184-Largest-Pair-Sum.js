e274e81d7738c8cef04f483839e7fc46dd902208
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-19 23:57:40 -0500

    chore: add refactor explanation to js file

--- CodeWars/184-Largest-Pair-Sum.js
+++ CodeWars/184-Largest-Pair-Sum.js
@@ -6,3 +6,13 @@ function largestPairSum(numbers) {
 
   return firstLargest + secondLargest;
 }
+
+/*
+
+REFACTORED SOLUTION -
+
+Rather than store the two values need in variables and removing 
+elements from the array, we sort the original array instead. 
+After sorting in descending order, we can just return the first 
+two elements of the adjusted array.
+*/
