52cc29848a06d58b7f8f66aff83715e50568cbb2
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-22 01:24:06 -0500

    chore: add solution code to js file

--- CodeWars/185-Not-All-But-Sometimes.js
+++ CodeWars/185-Not-All-But-Sometimes.js
@@ -1,1 +1,16 @@
-function remove(str, what) {}
+function remove(str, what) {
+  str = str.split('');
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (what[char] && what[char] > 0) {
+      str[i] = 0;
+      what[char]--;
+    }
+  }
+
+  str = str.filter((element) => element !== 0).join('');
+
+  return str.length > 0 ? str : '';
+}
