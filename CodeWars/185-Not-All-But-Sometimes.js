5b6d429c69781abe8ba085bf01dfb0181c2d9dc8
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-22 01:34:59 -0500

    chore: add refactor explanation to js file

--- CodeWars/185-Not-All-But-Sometimes.js
+++ CodeWars/185-Not-All-But-Sometimes.js
@@ -14,3 +14,16 @@ function remove(str, what) {
 
   return str.length > 0 ? str : '';
 }
+
+/*
+REFACTORED SOLUTION -
+
+Alternative to the for-loop iterating, we can still redefine the "str" 
+input as an array then map. Within mapping, we can just check truthiness 
+of the the character in the object, naturally checking that the values 
+are still above 0; only adding letters that exist as keys in the "what" 
+object when object lookup using that letters equals "0". All other 
+letters that are not keys in the "what" object still get added. These 
+letters are all added to a result variable that is intialized as an 
+empty string.
+*/
