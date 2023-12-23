7f8a7ca88874364dec9876f0aa1dd226b857339a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-22 23:40:28 -0500

    chore: add refactored solution code to js file

--- CodeWars/186-Digits-Average.js
+++ CodeWars/186-Digits-Average.js
@@ -25,3 +25,18 @@ because the numbers will not be push into the "tier" array as strings. At
 the very end of our function, we can make use of "number" and "join" to avoid 
 hard coding the return of the first element of the input array returned as output.
 */
+
+function digitsAverage(input) {
+  input = String(input).split('');
+
+  while (input.length > 1) {
+    let tier = [];
+
+    for (let i = 0; i < input.length - 1; i++) {
+      tier.push(Math.ceil((Number(input[i]) + Number(input[i + 1])) / 2));
+    }
+
+    input = tier;
+  }
+  return Number(input.join(''));
+}
