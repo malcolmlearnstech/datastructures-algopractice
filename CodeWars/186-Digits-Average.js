fb051d88ea52dc600170f6ebeea1f9c7dfd5aa4f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-22 23:39:28 -0500

    chore: add refactor explanation to js file

--- CodeWars/186-Digits-Average.js
+++ CodeWars/186-Digits-Average.js
@@ -14,3 +14,14 @@ function digitsAverage(input) {
   }
   return Number(input[0]);
 }
+
+/*
+REFACTORED SOLUTION -
+
+Because the elements are converted into numbers within the tier push scoped 
+in the for-loop, using toString is not necessary. Additionally, within the 
+for loop, we do not need to use the "join" method when we redefine input, 
+because the numbers will not be push into the "tier" array as strings. At 
+the very end of our function, we can make use of "number" and "join" to avoid 
+hard coding the return of the first element of the input array returned as output.
+*/
