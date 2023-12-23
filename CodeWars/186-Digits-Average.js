68d75384371a9d7c846fc1d189336eac3eaf7aaa
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-22 23:03:26 -0500

    chore: add solution code to js file

--- CodeWars/186-Digits-Average.js
+++ CodeWars/186-Digits-Average.js
@@ -1,1 +1,16 @@
-function digitsAverage(input) {}
+function digitsAverage(input) {
+  input = String(input).split('');
+
+  while (input.length > 1) {
+    let tier = [];
+
+    for (let i = 0; i < input.length - 1; i++) {
+      tier.push(
+        Math.ceil((Number(input[i]) + Number(input[i + 1])) / 2).toString()
+      );
+    }
+
+    input = tier.join('');
+  }
+  return Number(input[0]);
+}
