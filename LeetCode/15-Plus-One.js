d675107c0566ffd293c7cb3c48a0ae3929f02b0e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-24 23:02:12 -0500

    chore: add solution code to js file

--- LeetCode/15-Plus-One.js
+++ LeetCode/15-Plus-One.js
@@ -1,1 +1,27 @@
-var plusOne = function (digits) {};
+var plusOne = function (digits) {
+  digits[digits.length - 1] = digits[digits.length - 1] + 1;
+
+  if (digits.length === 1 && digits[0] > 9) {
+    digits[0] = 0;
+    digits.unshift(1);
+  }
+
+  if (digits[digits.length - 1] > 9) {
+    digits[digits.length - 1] = 0;
+
+    for (let i = digits.length - 2; i >= 0; i--) {
+      digits[i]++;
+      if (digits[i] <= 9) {
+        break;
+      } else {
+        digits[i] = 0;
+      }
+    }
+  }
+
+  if (digits[0] === 0) {
+    digits.unshift(1);
+  }
+
+  return digits;
+};
