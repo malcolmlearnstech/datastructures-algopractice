c07d24823c3ad7092577116cb2703010f1d9c1d7
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-26 18:38:40 -0500

    chore: add solution code to js file

--- CodeWars/188-Easy-Time-Convert.js
+++ CodeWars/188-Easy-Time-Convert.js
@@ -1,1 +1,13 @@
-function timeConvert(num) {}
+function timeConvert(num) {
+  if (num <= 0) {
+    return '00:00';
+  }
+
+  let hours = String(Math.floor(num / 60));
+  let mins = String(num % 60);
+
+  hours = hours.length > 1 ? hours : '0' + hours;
+  mins = mins.length > 1 ? mins : '0' + mins;
+
+  return `${hours}:${mins}`;
+}
