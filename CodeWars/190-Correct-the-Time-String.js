9c35872ad6d62ac6df7f41d9ba1082d91f1d4ac5
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-28 23:59:06 -0500

    chore: add solution code to js file

--- CodeWars/190-Correct-the-Time-String.js
+++ CodeWars/190-Correct-the-Time-String.js
@@ -1,1 +1,31 @@
-function timeCorrect(timestring) {}
+function timeCorrect(timestring) {
+  if (timestring === null) {
+    return null;
+  } else if (timestring === '') {
+    return '';
+  } else if (timestring.match(/[^0-9:]/g) || !timestring.match(/[:]/g)) {
+    return null;
+  }
+
+  timestring = timestring.split(':').map((element) => Number(element));
+
+  if (timestring[2] > 59) {
+    timestring[1] = timestring[1] + Math.floor(timestring[2] / 60);
+    timestring[2] = timestring[2] - 60;
+  }
+
+  if (timestring[1] > 59) {
+    timestring[0] = timestring[0] + Math.floor(timestring[1] / 60);
+    timestring[1] = timestring[1] - 60;
+  }
+
+  if (timestring[0] >= 24) {
+    while (timestring[0] >= 24) {
+      timestring[0] -= 24;
+    }
+  }
+
+  return timestring
+    .map((element) => String(element).padStart(2, '0'))
+    .join(':');
+}
