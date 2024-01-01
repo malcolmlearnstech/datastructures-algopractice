dcf46f9b539e4d144f687c5204fb359c7ec85241
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-12-31 23:46:27 -0500

    chore: add solution code to js file

--- CodeWars/192-Find-the-Mine.js
+++ CodeWars/192-Find-the-Mine.js
@@ -1,1 +1,16 @@
-function mineLocation(field) {}
+function mineLocation(field) {
+  let row = 0;
+
+  for (let i = 0; i < field.length; i++) {
+    let currentRow = field[i];
+    let column = 0;
+
+    for (let j = 0; j < currentRow.length; j++) {
+      if (currentRow[j] === 1) {
+        return [row, column];
+      }
+      column++;
+    }
+    row++;
+  }
+}
