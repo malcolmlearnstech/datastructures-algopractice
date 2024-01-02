ed24ca741a95792f2bddad0d3fa3bc2d1530d307
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-01 23:45:58 -0500

    chore: add solution code to js file

--- CodeWars/193-Filter-the-Number.js
+++ CodeWars/193-Filter-the-Number.js
@@ -1,1 +1,10 @@
-var filterString = function (value) {};
+var filterString = function (value) {
+  let nums = '';
+
+  value = value.split('').filter((element) => element.match(/[^a-zA-Z]/g));
+
+  for (const element of value) {
+    nums += element;
+  }
+  return parseInt(nums);
+};
