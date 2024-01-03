c3fd5687e28e100e1e4b97885ff5b3b57a6d97b8
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-02 23:57:32 -0500

    chore: add solution code to js file

--- CodeWars/194-Friday-the-13th.js
+++ CodeWars/194-Friday-the-13th.js
@@ -1,1 +1,10 @@
-function killCount(counselors, jason) {}
+function killCount(counselors, jason) {
+  let survivors = [];
+
+  counselors = counselors.filter((element) => element[1] < jason);
+
+  for (const element of counselors) {
+    survivors.push(element[0]);
+  }
+  return survivors;
+}
