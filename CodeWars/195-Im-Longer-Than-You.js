2693bcbe45e9d3629c580fd5d57d92c542204834
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-04 00:54:59 -0500

    chore: add refactor explanation to js file

--- CodeWars/195-Im-Longer-Than-You.js
+++ CodeWars/195-Im-Longer-Than-You.js
@@ -5,3 +5,15 @@ function longer(s) {
     .sort((a, b) => a.length - b.length)
     .join(' ');
 }
+
+/*
+REFACTORED SOLUTION - 
+
+In a more extensive manner, we can build out an object with string length counts 
+as the keys. The corresponding values for each key would be an array housing 
+strings from the input of that key's length. We can then traverse through each 
+key-value pair, ultimately sorting the strings if housing more than one; finally 
+allowing us to push the elements within the values to an "orderedString" array. 
+Lastly, we return the newly ordered string from the "orderedString" array, using 
+the join() method.
+*/
