fd64697a8dca4afeacfd4e7fbbaf7a6f2e5304df
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-05 23:30:06 -0500

    chore: add refactored solution code to js file

--- CodeWars/196-Filter-Long-Words.js
+++ CodeWars/196-Filter-Long-Words.js
@@ -11,3 +11,7 @@ For the sake of condensing or just making the solution into a one-liner,
 we can append the filter() logic to the split() applied to "sentence" 
 itself and return everything as a whole.
 */
+
+function filterLongWords(sentence, n) {
+  return sentence.split(' ').filter((element) => element.length > n);
+}
