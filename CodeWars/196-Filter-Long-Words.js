d71a79390dbf303ebf2d985b63c65adf04b34140
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-05 23:29:47 -0500

    chore: add refactor explanation to js file

--- CodeWars/196-Filter-Long-Words.js
+++ CodeWars/196-Filter-Long-Words.js
@@ -3,3 +3,11 @@ function filterLongWords(sentence, n) {
 
   return sentence.filter((element) => element.length > n);
 }
+
+/*
+REFACTORED SOLUTION -
+
+For the sake of condensing or just making the solution into a one-liner, 
+we can append the filter() logic to the split() applied to "sentence" 
+itself and return everything as a whole.
+*/
