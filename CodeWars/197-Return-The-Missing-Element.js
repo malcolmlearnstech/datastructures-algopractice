821d9e5f8b46df83e036d3e7afd4253d6a2aa28a
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-07 23:53:38 -0500

    chore: add refactor explanation to js file

--- CodeWars/197-Return-The-Missing-Element.js
+++ CodeWars/197-Return-The-Missing-Element.js
@@ -17,3 +17,13 @@ function getMissingElement(superImportantArray) {
     }
   }
 }
+
+/*
+REFACTORED SOLUTION -
+
+Instead all the conditionals, we can just count from 0 to 9, as the prompt 
+indicates all numbers that will be included. All we have to do from there is 
+check if the number we're currently iterating is included in the input array. 
+Which ever number that returns false is the number we will return at the end 
+of our function.
+*/
