3d9c8ba21a4a67b37b7e3410b2ec5d3ff086f51e
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-07 23:17:50 -0500

    chore: add solution code to js file

--- CodeWars/197-Return-The-Missing-Element.js
+++ CodeWars/197-Return-The-Missing-Element.js
@@ -1,1 +1,19 @@
-function getMissingElement(superImportantArray) {}
+function getMissingElement(superImportantArray) {
+  superImportantArray = superImportantArray.sort();
+
+  for (const element of superImportantArray) {
+    if (
+      !superImportantArray.includes(element + 1) &&
+      element + 1 >= 0 &&
+      element + 1 <= 9
+    ) {
+      return element + 1;
+    } else if (
+      !superImportantArray.includes(element - 1) &&
+      element - 1 >= 0 &&
+      element - 1 <= 9
+    ) {
+      return element - 1;
+    }
+  }
+}
