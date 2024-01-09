3f545c5041d823bc2731fc7d88aa884d25d4d0b2
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-08 23:53:40 -0500

    chore: add solution code to js file

--- CodeWars/198-Halving-Sum.js
+++ CodeWars/198-Halving-Sum.js
@@ -1,1 +1,11 @@
-function halvingSum(n) {}
+function halvingSum(n) {
+  let sum = n;
+  let divisor = 2;
+
+  while (divisor <= n) {
+    sum += Math.floor(n / divisor);
+    divisor *= 2;
+  }
+
+  return sum;
+}
