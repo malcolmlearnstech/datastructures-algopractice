16b9353630a1aec7b0c164947761e8af71713c8a
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-10 23:44:25 -0500

    chore: add solution code to js file

--- CodeWars/199-Maximum-Product.js
+++ CodeWars/199-Maximum-Product.js
@@ -1,1 +1,9 @@
-function adjacentElementsProduct(array) {}
+function adjacentElementsProduct(array) {
+  let max = -Infinity;
+
+  for (let i = 0; i < array.length - 1; i++) {
+    max = Math.max(max, array[i] * array[i + 1]);
+  }
+
+  return max;
+}
