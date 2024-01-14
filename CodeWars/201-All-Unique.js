dc2cd47cdae679dae3f4e6ecad4fb3084bc262d9
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-13 23:36:33 -0500

    chore: add refactored solution code to js file

--- CodeWars/201-All-Unique.js
+++ CodeWars/201-All-Unique.js
@@ -19,3 +19,19 @@ REFACTORED SOLUTION -
 
 Because the .every() method inherently returns true or false, using a ternary is not necessary.
 */
+
+function hasUniqueChars(str) {
+  let frequency = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (!frequency[str[i]]) {
+      frequency[str[i]] = 1;
+    } else frequency[str[i]]++;
+  }
+
+  frequency = Object.values(frequency);
+
+  let isOne = (element) => element === 1;
+
+  return frequency.every(isOne);
+}
