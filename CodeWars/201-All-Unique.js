a22ecf582924ecff1e445bc983246e114d5b6d6c
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-13 23:34:50 -0500

    chore: add solution code to js file

--- CodeWars/201-All-Unique.js
+++ CodeWars/201-All-Unique.js
@@ -1,1 +1,15 @@
-function hasUniqueChars(str) {}
+function hasUniqueChars(str) {
+  let frequency = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (!frequency[str[i]]) {
+      frequency[str[i]] = 1;
+    } else frequency[str[i]]++;
+  }
+
+  let isOne = (element) => element === 1;
+
+  frequency = Object.values(frequency);
+
+  return frequency.every(isOne) ? true : false;
+}
