e55925465e2c28e125338cf1763b289eb9d62770
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-16 23:47:02 -0500

    chore: add solution code to js file

--- CodeWars/203-Split-in-Parts.js
+++ CodeWars/203-Split-in-Parts.js
@@ -1,1 +1,12 @@
-var splitInParts = function (s, partLength) {};
+var splitInParts = function (s, partLength) {
+  let array = [];
+  s = s.split('');
+
+  while (s.length > 0) {
+    let chunk = s.slice(0, partLength).join('');
+    array.push(chunk);
+    s.splice(0, partLength);
+  }
+
+  return array.join(' ');
+};
