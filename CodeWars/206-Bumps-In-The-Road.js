8b8ff0cb1bad9b2f3c39a78765b13d577a5d1c9a
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-19 23:47:13 -0500

    chore: add solution code to js file

--- CodeWars/206-Bumps-In-The-Road.js
+++ CodeWars/206-Bumps-In-The-Road.js
@@ -1,1 +1,11 @@
-function bump(x) {}
+function bump(x) {
+  let bumpCount = 0;
+
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === 'n') {
+      bumpCount++;
+    }
+  }
+
+  return bumpCount > 15 ? 'Car Dead' : 'Woohoo!';
+}
