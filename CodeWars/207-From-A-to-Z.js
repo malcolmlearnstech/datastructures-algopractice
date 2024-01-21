fa618a6a5f675a65d927c15177eb351846d9afba
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-20 23:57:57 -0500

    chore: add solution refactor explanation to js file

--- CodeWars/207-From-A-to-Z.js
+++ CodeWars/207-From-A-to-Z.js
@@ -11,3 +11,15 @@ function gimmeTheLetters(sp) {
 
   return sp[0] === sp[0].toLowerCase() ? output : output.toUpperCase();
 }
+
+/*REFACTORED SOLUTION -
+
+We can use the charCodeAt() method, to create start and endpoints, because as we 
+iterate in our loop - the proper letters will follow one another in the same 
+alphabetical sequence due to character coding. Within the loop, we make sure to 
+use the fromCharCode() method to turn the codes back into the corresponding 
+letters before adding them to the input string. Because we are using character 
+codes and examining the actual letter characters themselves, we don't have to worry 
+about the casing, as lower case and upper case letters exist in different sets of 
+character codes.
+*/
