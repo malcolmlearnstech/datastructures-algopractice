22eedc8efedd2f5d5d59419cfccd9c5c357d2ad3
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-20 23:45:23 -0500

    chore: add solution code to js file

--- CodeWars/207-From-A-to-Z.js
+++ CodeWars/207-From-A-to-Z.js
@@ -1,1 +1,13 @@
-function gimmeTheLetters(sp) {}
+function gimmeTheLetters(sp) {
+  let output = '';
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let endpoints = sp.split('-').map((element) => element.toLowerCase());
+  let idx = alphabet.indexOf(endpoints[0]);
+
+  while (idx <= alphabet.indexOf(endpoints[1])) {
+    output += alphabet[idx];
+    idx++;
+  }
+
+  return sp[0] === sp[0].toLowerCase() ? output : output.toUpperCase();
+}
