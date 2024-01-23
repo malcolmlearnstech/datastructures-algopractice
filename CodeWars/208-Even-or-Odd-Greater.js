bc1d5248d29b6b0889b41790c13293b3744c4fa6
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-22 23:58:49 -0500

    chore: add solution code to js file

--- CodeWars/208-Even-or-Odd-Greater.js
+++ CodeWars/208-Even-or-Odd-Greater.js
@@ -1,1 +1,16 @@
-function evenOrOdd(str) {}
+function evenOrOdd(str) {
+  str = str.split('').map((element) => Number(element));
+
+  let even = str
+    .filter((element) => element % 2 === 0)
+    .reduce((acc, currentVal) => acc + currentVal, 0);
+  let odd = str
+    .filter((element) => element % 2 !== 0)
+    .reduce((acc, currentVal) => acc + currentVal, 0);
+
+  return even > odd
+    ? 'Even is greater than Odd'
+    : odd > even
+    ? 'Odd is greater than Even'
+    : 'Even and Odd are the same';
+}
