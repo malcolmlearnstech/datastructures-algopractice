a0c82b59049a193678c36eb85f8bd0308babfacf
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-23 23:07:43 -0500

    chore: add refactor explanation to js file

--- CodeWars/209-Substring-Fun.js
+++ CodeWars/209-Substring-Fun.js
@@ -9,3 +9,12 @@ function nthChar(words) {
 
   return string;
 }
+
+/*
+REFACTORED SOLUTION -
+
+Rather than creating a separate variable, we can employ the map() method which 
+will inherently build out an array. Once map() is used, we employ the join() 
+method. This can all be done within a ternary operator, with the determing 
+condition staying the same as the if conditional from our original solution code.
+*/
