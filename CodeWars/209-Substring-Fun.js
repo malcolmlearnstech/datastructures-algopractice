2f297a8e932a3e3ced915d91633a119cc21ca4e7
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-23 22:43:49 -0500

    chore: add solution code to js file

--- CodeWars/209-Substring-Fun.js
+++ CodeWars/209-Substring-Fun.js
@@ -1,1 +1,11 @@
-function nthChar(words) {}
+function nthChar(words) {
+  let string = '';
+
+  if (words.length >= 1) {
+    for (let i = 0; i < words.length; i++) {
+      string += words[i][i];
+    }
+  }
+
+  return string;
+}
