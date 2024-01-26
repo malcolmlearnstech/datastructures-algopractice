8c486a6f9694a1fa186b585d718d8bb4e9a73779
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-25 23:41:33 -0500

    chore: add solution code to js file

--- CodeWars/210-Pair-of-Gloves.js
+++ CodeWars/210-Pair-of-Gloves.js
@@ -1,1 +1,15 @@
-function numberOfPairs(gloves) {}
+function numberOfPairs(gloves) {
+  let inventory = {};
+  let pairs = 0;
+
+  gloves.map((element) => {
+    !inventory[element] ? (inventory[element] = 1) : inventory[element]++;
+  });
+
+  for (const key in inventory) {
+    inventory[key] % 2 === 0
+      ? (pairs += inventory[key] / 2)
+      : (pairs += Math.floor(inventory[key] / 2));
+  }
+  return pairs;
+}
