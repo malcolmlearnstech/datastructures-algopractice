fcdb46fd764c6d3afcfa23db2417ffc2d5a63373
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-26 23:56:41 -0500

    chore: add solution code to js file

--- CodeWars/211-Nth-Power-Rules-Them-All.js
+++ CodeWars/211-Nth-Power-Rules-Them-All.js
@@ -1,1 +1,11 @@
-function modifiedSum(a, n) {}
+function modifiedSum(a, n) {
+  let exponentSum = 0;
+  let regularSum = 0;
+
+  a.map((element) => {
+    exponentSum += Math.pow(element, n);
+    regularSum += element;
+  });
+
+  return exponentSum - regularSum;
+}
