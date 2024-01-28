3eb290fa3ef205505f654df95a4e156227566c20
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-27 22:40:53 -0500

    chore: add refactored solution code to js file

--- CodeWars/211-Nth-Power-Rules-Them-All.js
+++ CodeWars/211-Nth-Power-Rules-Them-All.js
@@ -15,3 +15,10 @@ REFACTORED SOLUTION -
 
 Instead of using the map() method to iterate through, within the "exponentSum" & "regularSum" variables, we can apply the reduce() method and lastly caluculate the difference.
 */
+
+function modifiedSum(a, n) {
+  let regularSum = a.reduce((acc, current) => acc + current, 0);
+  let exponentSum = a.reduce((acc, current) => acc + Math.pow(current, n), 0);
+
+  return exponentSum - regularSum;
+}
