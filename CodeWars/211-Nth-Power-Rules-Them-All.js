4c118a7708bfafcea325100bb4ba38e8ae49ca25
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-01-28 23:51:07 -0500

    chore: add second refactor explanation to js file

--- CodeWars/211-Nth-Power-Rules-Them-All.js
+++ CodeWars/211-Nth-Power-Rules-Them-All.js
@@ -13,7 +13,9 @@ function modifiedSum(a, n) {
 /*
 REFACTORED SOLUTION -
 
-Instead of using the map() method to iterate through, within the "exponentSum" & "regularSum" variables, we can apply the reduce() method and lastly caluculate the difference.
+Instead of using the map() method to iterate through, within the "exponentSum" 
+& "regularSum" variables, we can apply the reduce() method and lastly 
+caluculate the difference.
 */
 
 function modifiedSum(a, n) {
@@ -22,3 +24,13 @@ function modifiedSum(a, n) {
 
   return exponentSum - regularSum;
 }
+
+/*
+REFACTORED SOLUTION -
+
+Alternative to implementing the reduce() method twice, and storing inside of variables, we can make use of the reduce() method once. We can apply the 
+logic used in the "regularSum" variable, the main would be within the logic 
+for "current". While adding the current element to the third power, we can simultaneously subtract the single value of the element. Setting up reduce() 
+in that way allows for us do both required additions and subtractions in 
+one method.
+*/
