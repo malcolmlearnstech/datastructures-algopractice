2420fcae6648fef3466bff90834004aa4de14968
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-04 23:56:42 -0500

    chore: add refactor explanation to js file

--- CodeWars/213-Offload-Your-Work.js
+++ CodeWars/213-Offload-Your-Work.js
@@ -11,3 +11,9 @@ function workNeeded(projectMinutes, freelancers) {
     return `I need to work ${hours} hour(s) and ${minutes} minute(s)`;
   }
 }
+
+/*
+REFACTORED SOLUTION -
+
+We can use a ternary operator instead of the if...else logic. Additionaly, rather than creating new variables ("hours" and "minutes"), we can employ the logic for them within the second result of our ternary operator.
+*/
