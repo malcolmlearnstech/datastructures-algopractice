e46da684178da2b95396fadd2002d86772e58d36
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-04 23:13:39 -0500

    chore: add solution code to js file

--- CodeWars/213-Offload-Your-Work.js
+++ CodeWars/213-Offload-Your-Work.js
@@ -1,1 +1,13 @@
-function workNeeded(projectMinutes, freelancers) {}
+function workNeeded(projectMinutes, freelancers) {
+  freelancers.map((element) => {
+    projectMinutes -= element[0] * 60 + element[1];
+  });
+
+  if (projectMinutes < 1) {
+    return 'Easy Money!';
+  } else {
+    let hours = Math.floor(projectMinutes / 60);
+    let minutes = projectMinutes % 60;
+    return `I need to work ${hours} hour(s) and ${minutes} minute(s)`;
+  }
+}
