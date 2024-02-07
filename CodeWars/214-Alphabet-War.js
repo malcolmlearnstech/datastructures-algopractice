a7f081151bf99366c315a8098c63572e7a39e663
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-06 23:57:48 -0500

    chore: add solution code to js file

--- CodeWars/214-Alphabet-War.js
+++ CodeWars/214-Alphabet-War.js
@@ -1,1 +1,25 @@
-function alphabetWar(fight) {}
+function alphabetWar(fight) {
+  let values = {
+    w: -4,
+    p: -3,
+    b: -2,
+    s: -1,
+    m: 4,
+    q: 3,
+    d: 2,
+    z: 1,
+  };
+  let count = 0;
+
+  for (let i = 0; i < fight.length; i++) {
+    if (values[fight[i]]) {
+      count += values[fight[i]];
+    }
+  }
+
+  return count > 0
+    ? 'Right side wins!'
+    : count < 0
+    ? 'Left side wins!'
+    : "Let's fight again!";
+}
