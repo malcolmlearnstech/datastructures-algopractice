c2a52ce79f605bd1d081b07f02a940510153abe7
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-07 22:06:05 -0500

    chore: add solution code to js file

--- CodeWars/215-If-You-Can-Read-This.js
+++ CodeWars/215-If-You-Can-Read-This.js
@@ -1,1 +1,18 @@
-function toNAto(words) {}
+function toNato(words) {
+  let translatedString = '';
+
+  words
+    .split('')
+    .filter((element) => element !== ' ')
+    .map((element) => {
+      if (NATO[element.toUpperCase()]) {
+        translatedString += NATO[element.toUpperCase()];
+      } else translatedString += element;
+
+      if (element !== ' ') {
+        translatedString += ' ';
+      }
+    });
+
+  return translatedString.trimEnd();
+}
