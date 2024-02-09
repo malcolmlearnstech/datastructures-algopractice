0a3b9711bb1a0c36880428c5273c55fa71c5a71e
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-08 23:37:43 -0500

    Chore: add initial solution code to js file

--- CodeWars/216-Range-Bit-Counting.js
+++ CodeWars/216-Range-Bit-Counting.js
@@ -1,1 +1,15 @@
-function rangeBitCount(a, b) {}
+function rangeBitCount(a, b) {
+  let fromAToB = [];
+  let oneCounter = 0;
+
+  for (let i = a; i <= b; i++) {
+    fromAToB.push(i.toString(2));
+  }
+
+  fromAToB.map(
+    (element) =>
+      (oneCounter += element.split('').filter((e) => e === '1').length)
+  );
+
+  return oneCounter;
+}
