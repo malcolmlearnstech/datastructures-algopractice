ee0156b74002756649a6ab3415bf84e3fcde9c9d
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-08 23:51:05 -0500

    Chore: add explanation of code refactor to js file

--- CodeWars/216-Range-Bit-Counting.js
+++ CodeWars/216-Range-Bit-Counting.js
@@ -13,3 +13,13 @@ function rangeBitCount(a, b) {
 
   return oneCounter;
 }
+
+/*
+REFACTORED SOLUTION -
+
+Instead of having to create an array and then mapping thru that array, we can create 
+a singular new variable, using only "oneCounter". Still applying the "toString(2)" 
+logic within the for-loop, we can emply the replace() method; removing all 0's as 
+binary only uses 1s and 0s. Then we simply use ".length" to check the number of 1s 
+and add that count to our "oneCounter" variable.
+*/
