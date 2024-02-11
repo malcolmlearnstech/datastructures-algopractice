67439bf2d8882aecade92893e0620d93aa217359
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-10 22:25:11 -0500

    Chore: add solution code to js file

--- CodeWars/217-Square-Even-Root-Odd.js
+++ CodeWars/217-Square-Even-Root-Odd.js
@@ -1,1 +1,11 @@
-const sumSquareEvenRootOdd = (ns) => {};
+const sumSquareEvenRootOdd = (ns) => {
+  let newList = ns.map((element) => {
+    if (element % 2 === 0) {
+      return Math.pow(element, 2);
+    } else return Math.sqrt(element);
+  });
+
+  return Number(
+    newList.reduce((acc, currentVal) => acc + currentVal).toFixed(2)
+  );
+};
