3f65b01d27ed9f6eb2a1282aa3a29bbb5c962999
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-10 22:28:41 -0500

    Chore: add refactor explanation to js file

--- CodeWars/217-Square-Even-Root-Odd.js
+++ CodeWars/217-Square-Even-Root-Odd.js
@@ -9,3 +9,12 @@ const sumSquareEvenRootOdd = (ns) => {
     newList.reduce((acc, currentVal) => acc + currentVal).toFixed(2)
   );
 };
+
+/*
+REFACTORED SOLUTION -
+
+To shorten the logic laid out within our map(), we can employ the ternary operator, 
+which will perform identically to the logic of the if-else setup. Additionally at 
+our final return, we can employ the Unary Operator("+"), to convert the result our 
+reduce() and appeneded toFixed() back into a number from a string.
+*/
