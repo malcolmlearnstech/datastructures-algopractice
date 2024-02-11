98ad621ea767d34c581f705d109c1b43d07447d2
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-10 22:34:34 -0500

    Chore: add second refactor explanation to js file

--- CodeWars/217-Square-Even-Root-Odd.js
+++ CodeWars/217-Square-Even-Root-Odd.js
@@ -26,3 +26,13 @@ function sumSquareEvenRootOdd(ns) {
 
   return +newList.reduce((acc, currentVal) => acc + currentVal).toFixed(2);
 }
+
+/*
+SECOND REFACTORED SOLUTION -
+
+To avoid creating a new variable ("newList"), we can append the logic used for reduce() 
+to the map() logic. This is because map() naturally returns array and we only need to 
+use reduce() from there to figure out the full sum. We can still use the unary operator 
+as well, it just gets moved to the very beginning of the return since everything is 
+appended.
+*/
