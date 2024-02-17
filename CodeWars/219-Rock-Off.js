bf9bb30b1528e45fb1a18261793d9ac77d67b4ab
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-16 23:46:07 -0500

    Chore: add solution code to js file

--- CodeWars/219-Rock-Off.js
+++ CodeWars/219-Rock-Off.js
@@ -1,1 +1,18 @@
-function solve(a, b) {}
+function solve(a, b) {
+  let aliceScore = 0;
+  let bobScore = 0;
+
+  a.map((element, idx) => {
+    if (a[idx] > b[idx]) {
+      aliceScore++;
+    } else if (a[idx] < b[idx]) {
+      bobScore++;
+    }
+  });
+
+  return aliceScore > bobScore
+    ? `${aliceScore}, ${bobScore}: Alice made "Kurt" proud!`
+    : aliceScore < bobScore
+    ? `${aliceScore}, ${bobScore}: Bob made "Jeff" proud!`
+    : `${aliceScore}, ${bobScore}: that looks like a "draw"! Rock on!`;
+}
