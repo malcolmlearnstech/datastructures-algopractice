2c31e020d22a8fb8b6d64d06302e88d39d5931fe
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-29 23:30:51 -0500

    Chore: add refactored solution code to js file

--- CodeWars/218-Ordered-Count-Characters.js
+++ CodeWars/218-Ordered-Count-Characters.js
@@ -24,3 +24,23 @@ the key-value pairs are output in the order the characters appear, we can perfor
 string. As we iterate thru the string, and push the key-values to our empty array, we then delete the object 
 entry that we just added. This ensures that there are no repeats within our output array. 
 */
+
+function orderedCount(text) {
+  let var1 = {};
+  let var2 = [];
+
+  for (let i = 0; i < text.length; i++) {
+    if (!var1[text[i]]) {
+      var1[text[i]] = 1;
+    } else var1[text[i]]++;
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    if (var1[text[i]]) {
+      var2.push([text[i], var1[text[i]]]);
+      delete var1[text[i]];
+    }
+  }
+
+  return var2;
+}
