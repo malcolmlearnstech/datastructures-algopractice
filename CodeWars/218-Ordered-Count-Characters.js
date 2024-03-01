88bb6bb1a0851b445cb082edb7785f1b36f91df1
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-02-29 23:29:50 -0500

    Chore: add refactor explanation to js file

--- CodeWars/218-Ordered-Count-Characters.js
+++ CodeWars/218-Ordered-Count-Characters.js
@@ -14,3 +14,13 @@ const orderedCount = function (text) {
 
   return chronological;
 };
+
+/*
+REFACTORED SOLUTION -
+
+If you are unable to remember the syntax for use of Map() object, you can emply empty object and empty array. 
+You first build out the object that will hold a key-value pairs related to character frequency. To ensure that 
+the key-value pairs are output in the order the characters appear, we can perform another loop thru the input 
+string. As we iterate thru the string, and push the key-values to our empty array, we then delete the object 
+entry that we just added. This ensures that there are no repeats within our output array. 
+*/
