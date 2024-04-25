4468e920f29aa4b844fe1ae7b14c6b8425e50a3c
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-04-25 13:26:44 -0400

    Chore: add refactored solution code to js file

--- CodeWars/222-How-Old-Will-I-Be.js
+++ CodeWars/222-How-Old-Will-I-Be.js
@@ -16,3 +16,15 @@ REFACTORED SOLUTION -
 For better readability and ease of maintanance, we can edit the nested conditionals 
 to be ternary operators instead, shortening the code.
 */
+
+function calculateAge(birthYear, randomYear) {
+  if (Math.abs(randomYear - birthYear) === 1) {
+    return birthYear < randomYear
+      ? 'You are 1 year old.'
+      : 'You will be born in 1 year.';
+  } else if (Math.abs(randomYear - birthYear) > 1) {
+    return birthYear < randomYear
+      ? `You are ${randomYear - birthYear} years old.`
+      : `You will be born in ${birthYear - randomYear} years.`;
+  } else return 'You were born this very year!';
+}
