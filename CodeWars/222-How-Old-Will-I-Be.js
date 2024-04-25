1fceb54ce6735bf76f47610758c0b9921cec8189
Malcolm Mack <malcolmtechlearning@gmail.com>
2024-04-25 13:15:09 -0400

    Chore: add solution code to js file

--- CodeWars/222-How-Old-Will-I-Be.js
+++ CodeWars/222-How-Old-Will-I-Be.js
@@ -1,1 +1,11 @@
-function calculateAge(birthYear, randomYear) {}
+function calculateAge(birthYear, randomYear) {
+  if (Math.abs(randomYear - birthYear) === 1) {
+    if (birthYear < randomYear) {
+      return 'You are 1 year old.';
+    } else return 'You will be born in 1 year.';
+  } else if (Math.abs(randomYear - birthYear) > 1) {
+    if (birthYear < randomYear) {
+      return `You are ${randomYear - birthYear} years old.`;
+    } else return `You will be born in ${birthYear - randomYear} years.`;
+  } else return 'You were born this very year!';
+}
