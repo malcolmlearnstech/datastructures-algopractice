3fd243b29bcbe901b55cca2d4429a98b0d14d4a1
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-03 23:34:06 -0500

    Chore: add refactor explanation to js file

--- CodeWars/225-Hello-Name-or-World.js
+++ CodeWars/225-Hello-Name-or-World.js
@@ -3,3 +3,10 @@ function hello(name) {
     ? `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
     : 'Hello, World!';
 }
+
+/*
+REFACTORED SOLUTION -
+
+For even more efficiency/precision the entire ternary can be placed with the JSX reference, 
+deciding whether a name will be placed or "World" will be, then continuing the template literal.
+*/
