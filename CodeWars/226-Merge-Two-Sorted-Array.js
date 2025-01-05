ba8bfac45f9f5791dac64253af63cf219efb715d
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-04 23:50:24 -0500

    Chore: add solution code to js file

--- CodeWars/226-Merge-Two-Sorted-Array.js
+++ CodeWars/226-Merge-Two-Sorted-Array.js
@@ -1,1 +1,11 @@
-function mergeArrays(arr1, arr2) {}
+function mergeArrays(arr1, arr2) {
+  let merge = [];
+  arr1.map((element) => merge.push(element));
+  arr2.map((element) => {
+    if (!merge.includes(element)) {
+      merge.push(element);
+    }
+  });
+
+  return merge.sort((a, b) => a - b);
+}
