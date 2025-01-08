dc19d1dec043a848cbba69229f596cd123b08af7
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-07 23:52:38 -0500

    Chore: add refactored solution code to js file

--- CodeWars/227-USD-to-CNY.js
+++ CodeWars/227-USD-to-CNY.js
@@ -10,3 +10,9 @@ logic from the template literal. This allows a shorter line in the final
 return as only 'usd' has to be referred to in the JSX section of the 
 template literal.
 */
+
+function usdcny(usd) {
+  usd = (usd * 6.75).toFixed(2);
+
+  return `${usd} Chinese Yuan`;
+}
