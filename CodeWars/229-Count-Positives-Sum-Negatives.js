e4b36138dba29642513f7db513f0e64de00cc2a2
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-09 22:14:34 -0500

    Chore: add solution code to js file

--- CodeWars/229-Count-Positives-Sum-Negatives.js
+++ CodeWars/229-Count-Positives-Sum-Negatives.js
@@ -1,1 +1,14 @@
-function countPositivesSumNegatives(input) {}
+function countPositivesSumNegatives(input) {
+  let positives = 0;
+  let negatives = 0;
+
+  if (input === null || input.length < 1) {
+    return [];
+  }
+
+  input.map((element) => {
+    Math.sign(element) === 1 ? positives++ : (negatives += element);
+  });
+
+  return [positives, negatives];
+}
