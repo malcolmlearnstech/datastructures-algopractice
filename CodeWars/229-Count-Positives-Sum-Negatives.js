9e4a2d8d633a2b0cc5b1eb663654d68faf4bea7d
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-09 22:17:18 -0500

    Chore: add refactored solution code with refactor explanation

--- CodeWars/229-Count-Positives-Sum-Negatives.js
+++ CodeWars/229-Count-Positives-Sum-Negatives.js
@@ -12,3 +12,26 @@ function countPositivesSumNegatives(input) {
 
   return [positives, negatives];
 }
+
+/*
+REFACTORED SOLUTION -
+
+To keep all of the full methods contained on one space for context, 
+account for the potential outliers first as an if condition; 
+then perform the rest within the context of the else portion.
+*/
+
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length < 1) {
+    return [];
+  } else {
+    let positives = 0;
+    let negatives = 0;
+
+    input.map((element) => {
+      Math.sign(element) === 1 ? positives++ : (negatives += element);
+    });
+
+    return [positives, negatives];
+  }
+}
