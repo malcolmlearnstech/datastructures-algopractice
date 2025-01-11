0efec099709c99ff067b15ff2a8f676e98a4311e
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-10 21:43:28 -0500

    Chore: add refactored solution code to js file

--- CodeWars/230-Growth-of-a-Population.js
+++ CodeWars/230-Growth-of-a-Population.js
@@ -15,3 +15,11 @@ REFACTORED SOLUTION -
 
 Try out implementing pretty much the same logic but using a for-loop instead.
 */
+
+function nbYear(p0, percent, aug, p) {
+  let yearCount = 0;
+  for (let i = yearCount; p0 < p; yearCount++) {
+    p0 = p0 + Math.floor(p0 * (percent / 100)) + aug;
+  }
+  return yearCount;
+}
