10214a7c38300f20e105599566594a757b7c735e
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-10 21:40:24 -0500

    Chore: add solution code to js file

--- CodeWars/230-Growth-of-a-Population.js
+++ CodeWars/230-Growth-of-a-Population.js
@@ -1,3 +1,11 @@
 function nbYear(p0, percent, aug, p) {
-  // your code
+  let yearCount = 0;
+  percent = percent / 100;
+
+  while (p0 < p) {
+    yearCount++;
+    p0 += Math.floor(p0 * percent) + aug;
+  }
+
+  return yearCount;
 }
