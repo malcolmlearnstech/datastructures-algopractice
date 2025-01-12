8141fec765b334522eff6092595a086b2d0ae93f
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-11 23:04:26 -0500

    Chore: add solution code to js file

--- CodeWars/231-Opposites-Attract.js
+++ CodeWars/231-Opposites-Attract.js
@@ -1,1 +1,13 @@
-function lovefunc(flower1, flower2) {}
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 1) {
+    if (flower2 % 2 === 0) {
+      return true;
+    }
+    return false;
+  } else {
+    if (flower2 % 2 === 1) {
+      return true;
+    }
+    return false;
+  }
+}
