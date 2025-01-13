275e59fd446cc48302d4a30bb9981de5444f2dee
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-12 23:33:15 -0500

    Chore: add solution code to js file

--- CodeWars/232-Simple-Validation-of-Username-Regex.js
+++ CodeWars/232-Simple-Validation-of-Username-Regex.js
@@ -1,1 +1,11 @@
-function validateUsr(username) {}
+function validateUsr(username) {
+  let outliers = /[^a-z0-9_]/g;
+
+  return username.length > 16
+    ? false
+    : username.length < 4
+    ? false
+    : username.match(outliers)
+    ? false
+    : true;
+}
