9e4f26a57fa1ec5f0c362e5d13f5f855998833fa
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-13 23:25:32 -0500

    Chore: add solution code to js file

--- CodeWars/233-Correct-Mistakes-of-Character-Recognition.js
+++ CodeWars/233-Correct-Mistakes-of-Character-Recognition.js
@@ -1,1 +1,16 @@
-function correct(string) {}
+function correct(string) {
+  string = string.split('');
+  let correct = '';
+
+  string.map((element) => {
+    correct +=
+      element === '5'
+        ? 'S'
+        : element === '0'
+        ? 'O'
+        : element === '1'
+        ? 'I'
+        : element;
+  });
+  return correct;
+}
