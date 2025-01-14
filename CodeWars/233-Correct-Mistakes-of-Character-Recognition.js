f323bd8baf33c9f34f2e09475df5c21d100ef625
Malcolm Mack <malcolmtechlearning@gmail.com>
2025-01-13 23:30:24 -0500

    Chore: add explanation of refactor and refactored solution code to js file

--- CodeWars/233-Correct-Mistakes-of-Character-Recognition.js
+++ CodeWars/233-Correct-Mistakes-of-Character-Recognition.js
@@ -14,3 +14,19 @@ function correct(string) {
   });
   return correct;
 }
+
+/*
+REFACTORED SOLUTION -
+
+After some reading and refamiliarizing, seeing that the string.replace() 
+method can be applied. This require the need to create a new variable and 
+add the characters to a new variable while mapping. Using replace() allows 
+to return the string from the original input variable, going thru the 
+string and changing the characters matching your criteria, while 
+maintaining its original character order.
+
+*/
+
+function correct(string) {
+  return string.replace(/5/g, 'S').replace(/0/g, 'O').replace(/1/g, 'I');
+}
